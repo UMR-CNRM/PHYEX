1919b61dcbef67aa6f1e6b113e984d60d8b01309
Quentin Rodier <quentin.rodier@meteo.fr>
2021-11-08 11:57:41 +0100

    Quentin 08/11/2021: Merge AROME->COMMON tm06.f90 (minor)

--- src/common/turb/tm06.F90
+++ src/common/turb/tm06.F90
@@ -1,3 +1,7 @@
+!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
+!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!MNH_LIC for details. version 1.
 !     ######spl
       SUBROUTINE TM06(KKA,KKU,KKL,PTHVREF,PBL_DEPTH,PZZ,PSFTH,PMWTH,PMTH2)
       USE PARKIND1, ONLY : JPRB
