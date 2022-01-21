241f77c592f0e431191cbe1994bccbf27bf66fa3
Sébastien Riette <sebastien.riette@meteo.fr>
2022-01-21 17:07:39 +0100

    SR 21/01/2022 compute_frac_ice as fortran include
    mode_compute_frac_ice becomes an include file which must be used
    in contains section of subroutines through an #include or a fortran
    INCLUDE

--- src/common/micro/compute_frac_ice.func.h
+++ src/common/micro/compute_frac_ice.func.h
@@ -2,29 +2,13 @@
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
 !MNH_LIC for details. version 1.
-!     ######spl
-     MODULE MODE_COMPUTE_FRAC_ICE
-!    ############################
+      ELEMENTAL SUBROUTINE COMPUTE_FRAC_ICE(HFRAC_ICE,PFRAC_ICE,PT,KERR)
+
+! ******* TO BE INCLUDED IN THE *CONTAINS* OF A SUBROUTINE, IN ORDER TO EASE AUTOMATIC INLINING ******
+! => Don't use drHook !!!
 !
 !!****  *COMPUTE_FRAC_ICE* - computes ice fraction
-!!
-!!    PURPOSE
-!!    -------
-!!
-!!**  METHOD
-!!    ------
-!!    
-!!
-!!    EXTERNAL
-!!    --------
-!!
-!!    IMPLICIT ARGUMENTS
-!!    ------------------
-!!
-!!
-!!    REFERENCE
-!!    ---------
-!!
+!
 !!    AUTHOR
 !!    ------
 !!      Julien PERGAUD      * Meteo-France *
@@ -34,43 +18,21 @@
 !!      Original         13/03/06
 !!      S. Riette        April 2011 optimisation
 !!      S. Riette        08/2016 add option O
-!!      P. Wautelet 10/04/2019: replace ABORT and STOP calls by Print_msg
-!!      R. El Khatib 24-Aug-2021 Optimization by cache re-use + assume data is contiguous
-!!      S. Riette Jan-2022 Merge the 3 procedures in one module + array shape declaration
-!       R. El Khatib / S. Riette Jan-2022 written as an elemental subroutine
+!!      R. El Khatib     12-Aug-2021 written as a include file
 !
-! 
-!
-
-
-!****************** Don't use drHook !!!
-
-
-
-IMPLICIT NONE
-CONTAINS
-
-ELEMENTAL SUBROUTINE COMPUTE_FRAC_ICE(HFRAC_ICE, PFRAC_ICE, PT, KERR)
 !! --------------------------------------------------------------------------
-!       0. DECLARATIONS
-!          ------------
-!
 USE MODD_NEB, ONLY : XTMINMIX, XTMAXMIX
 USE MODD_CST, ONLY : XTT
 !
 IMPLICIT NONE
 !
+CHARACTER(LEN=1), INTENT(IN)    :: HFRAC_ICE       ! scheme to use
+REAL,             INTENT(IN)    :: PT              ! temperature
+REAL,             INTENT(INOUT) :: PFRAC_ICE       ! Ice fraction (1 for ice only, 0 for liquid only)
+INTEGER,          INTENT(OUT)   :: KERR            ! Error code in return
 !
-!*      0.1  declarations of arguments
-!
-CHARACTER(LEN=1),  INTENT(IN)    :: HFRAC_ICE ! scheme to use
-REAL,              INTENT(IN)    :: PT        ! Temperature
-REAL,              INTENT(INOUT) :: PFRAC_ICE ! Ice fraction (1 for ice only, 0 for liquid only)
-INTEGER,           INTENT(OUT)   :: KERR      ! Error code in return
-!
-!               0.2  declaration of local variables
-! 
 !------------------------------------------------------------------------
+
 !                1. Compute FRAC_ICE
 !
 KERR=0
@@ -87,7 +49,5 @@ SELECT CASE(HFRAC_ICE)
   CASE DEFAULT
     KERR=1
 END SELECT
-!
+
 END SUBROUTINE COMPUTE_FRAC_ICE
-!
-END MODULE MODE_COMPUTE_FRAC_ICE
