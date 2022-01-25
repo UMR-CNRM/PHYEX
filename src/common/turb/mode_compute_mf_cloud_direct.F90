af19b338abcb9f661cc700afb81058431f9fffe4
Sébastien Riette <sebastien.riette@meteo.fr>
2022-01-25 09:20:41 +0100

    Sébastien Riette 25/01/2022 merge compute_mf_cloud*

--- src/common/turb/mode_compute_mf_cloud_direct.F90
+++ src/common/turb/mode_compute_mf_cloud_direct.F90
@@ -3,34 +3,12 @@
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !MNH_LIC for details. version 1.
 !     ######spl
-     MODULE MODI_COMPUTE_MF_CLOUD_DIRECT
+     MODULE MODE_COMPUTE_MF_CLOUD_DIRECT
 !    ###################################
 !
-INTERFACE
-!     #################################################################
-      SUBROUTINE COMPUTE_MF_CLOUD_DIRECT(KKE, KKL, &
-                                        &KKLCL, PFRAC_UP, PRC_UP, PRI_UP,&
-                                        &PRC_MF, PRI_MF, PCF_MF)
-!     #################################################################
-!!
-!
-!*               1.1  Declaration of Arguments
-!
-INTEGER,                INTENT(IN)   :: KKE            ! uppest atmosphere physical index
-INTEGER,                INTENT(IN)   :: KKL            ! +1 if grid goes from ground to atmosphere top, -1 otherwise
-INTEGER, DIMENSION(:),  INTENT(IN)   :: KKLCL          ! index of updraft condensation level
-REAL, DIMENSION(:,:),   INTENT(IN)   :: PFRAC_UP       ! Updraft Fraction
-REAL, DIMENSION(:,:),   INTENT(IN)   :: PRC_UP,PRI_UP  ! updraft characteritics
-REAL, DIMENSION(:,:),   INTENT(OUT)  :: PRC_MF, PRI_MF ! cloud content (INPUT=environment, OUTPUT=conv. cloud)
-REAL, DIMENSION(:,:),   INTENT(OUT)  :: PCF_MF         ! and cloud fraction for MF scheme
-
-END SUBROUTINE COMPUTE_MF_CLOUD_DIRECT
-
-END INTERFACE
-!
-END MODULE MODI_COMPUTE_MF_CLOUD_DIRECT
-!     ######spl
-      SUBROUTINE COMPUTE_MF_CLOUD_DIRECT(KKE, KKL, &
+IMPLICIT NONE
+CONTAINS
+      SUBROUTINE COMPUTE_MF_CLOUD_DIRECT(KKB, KKE, KKL, &
                                         &KKLCL, PFRAC_UP, PRC_UP, PRI_UP,&
                                         &PRC_MF, PRI_MF, PCF_MF)
 !     #################################################################
@@ -69,16 +47,21 @@ END MODULE MODI_COMPUTE_MF_CLOUD_DIRECT
 !!    -------------
 !!      Original 25 Aug 2011
 !!      S. Riette Jan 2012: support for both order of vertical levels
+!!      S. Riette Apr 2013: computation begins one level lower (to be able to have a cloud
+!!                          on mass level just below the first saturated flux level)
 !! --------------------------------------------------------------------------
 !
 !*      0. DECLARATIONS
 !          ------------
 USE MODD_PARAM_MFSHALL_n, ONLY : XKCF_MF
+USE PARKIND1, ONLY : JPRB
+USE YOMHOOK , ONLY : LHOOK, DR_HOOK
 !
 IMPLICIT NONE
 !
 !*                    0.1  Declaration of Arguments
 !
+INTEGER,                INTENT(IN)   :: KKB            ! near ground physical index
 INTEGER,                INTENT(IN)   :: KKE            ! uppest atmosphere physical index
 INTEGER,                INTENT(IN)   :: KKL            ! +1 if grid goes from ground to atmosphere top, -1 otherwise
 INTEGER, DIMENSION(:),  INTENT(IN)   :: KKLCL          ! index of updraft condensation level
@@ -89,10 +72,12 @@ REAL, DIMENSION(:,:),   INTENT(OUT)  :: PCF_MF         ! and cloud fraction for 
 !
 !*                    0.1  Declaration of local variables
 !
-INTEGER  :: JI,JK
+INTEGER  :: JI,JK, JK0
+REAL(KIND=JPRB) :: ZHOOK_HANDLE
 !
 !*                    0.2 Initialisation
 !
+IF (LHOOK) CALL DR_HOOK('COMPUTE_MF_CLOUD_DIRECT',0,ZHOOK_HANDLE)
 !
 !*      1. COMPUTATION OF SUBGRID CLOUD
 !          ----------------------------
@@ -105,7 +90,10 @@ PRI_MF(:,:)=0.
 PCF_MF(:,:)=0.
 
 DO JI=1,SIZE(PCF_MF,1)
-  DO JK=KKLCL(JI),KKE-KKL,KKL
+  JK0=KKLCL(JI)-KKL ! first mass level with cloud
+  JK0=MAX(JK0, MIN(KKB,KKE)) !protection if KKL=1
+  JK0=MIN(JK0, MAX(KKB,KKE)) !protection if KKL=-1
+  DO JK=JK0,KKE-KKL,KKL
     PCF_MF(JI,JK ) = MAX( 0., MIN(1.,XKCF_MF *0.5* (       &
                 &    PFRAC_UP(JI,JK) +  PFRAC_UP(JI,JK+KKL) ) ))
     PRC_MF(JI,JK)  = 0.5* XKCF_MF * ( PFRAC_UP(JI,JK)*PRC_UP(JI,JK)  &
@@ -115,5 +103,7 @@ DO JI=1,SIZE(PCF_MF,1)
   END DO
 END DO
 
+IF (LHOOK) CALL DR_HOOK('COMPUTE_MF_CLOUD_DIRECT',1,ZHOOK_HANDLE)
 
 END SUBROUTINE COMPUTE_MF_CLOUD_DIRECT
+END MODULE MODE_COMPUTE_MF_CLOUD_DIRECT
