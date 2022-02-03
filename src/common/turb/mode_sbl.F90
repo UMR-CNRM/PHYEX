e769676769fc50dddfdd461747d3f448bd62535b
Sébastien Riette <sebastien.riette@meteo.fr>
2022-02-03 18:10:56 +0100

    Sébastien Riette 03/02/2022 turb directories cleaning
    Some merging
    Suppression of unused modules
    Move some modules in aux

--- src/common/turb/mode_sbl.F90
+++ src/common/turb/mode_sbl.F90
@@ -3,13 +3,8 @@
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !MNH_LIC for details. version 1.
 !-----------------------------------------------------------------
-!--------------- special set of characters for RCS information
-!-----------------------------------------------------------------
-! $Source$ $Revision$
-! MASDEV4_7 mode 2006/05/18 13:07:25
-!-----------------------------------------------------------------
-!     ############### 
-      MODULE MODE_SBL 
+!     ###############
+      MODULE MODE_SBL
 !     ###############
 !
 !!****  *MODE_SBL * - contains Surface Boundary Layer characteristics functions
@@ -19,24 +14,24 @@
 !
 !!**  METHOD
 !!    ------
-!!    
-!!    
+!!
+!!
 !!
 !!    EXTERNAL
 !!    --------
-!!       
+!!
 !!    IMPLICIT ARGUMENTS
-!!    ------------------ 
+!!    ------------------
 !!
 !!    REFERENCE
 !!    ---------
 !!
 !!    Businger et al 1971,   Wyngaard and Cote 1974
-!!      
+!!
 !!
 !!    AUTHOR
 !!    ------
-!!	V. Masson       * Meteo France *
+!!      V. Masson       * Meteo France *
 !!
 !!    MODIFICATIONS
 !!    -------------
@@ -44,6 +39,8 @@
 !!      V. Masson       06/11/02 optimization and add Businger fonction for TKE
 !!      V. Masson       01/01/03 use PAULSON_PSIM function
 !-----------------------------------------------------------------------------
+USE PARKIND1, ONLY : JPRB
+USE YOMHOOK , ONLY : LHOOK, DR_HOOK
 !
 !*       0.    DECLARATIONS
 !
@@ -88,11 +85,14 @@ FUNCTION BUSINGER_PHIM_3D(PZ_O_LMO)
   REAL, DIMENSION(SIZE(PZ_O_LMO,1), &
                   SIZE(PZ_O_LMO,2),SIZE(PZ_O_LMO,3)) :: BUSINGER_PHIM_3D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_3D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:,:,:) < 0. )
     BUSINGER_PHIM_3D(:,:,:) = (1.-15.*PZ_O_LMO)**(-0.25)
   ELSEWHERE
     BUSINGER_PHIM_3D(:,:,:) = 1. + 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_3D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIM_3D
 !
 !-------------------------------------------------------------------------------
@@ -101,11 +101,14 @@ FUNCTION BUSINGER_PHIM_2D(PZ_O_LMO)
   REAL, DIMENSION(:,:), INTENT(IN)                   :: PZ_O_LMO
   REAL, DIMENSION(SIZE(PZ_O_LMO,1),SIZE(PZ_O_LMO,2)) :: BUSINGER_PHIM_2D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_2D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:,:) < 0. )
     BUSINGER_PHIM_2D(:,:) = (1.-15.*PZ_O_LMO)**(-0.25)
   ELSEWHERE
     BUSINGER_PHIM_2D(:,:) = 1. + 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_2D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIM_2D
 !
 !-------------------------------------------------------------------------------
@@ -114,11 +117,14 @@ FUNCTION BUSINGER_PHIM_1D(PZ_O_LMO)
   REAL, DIMENSION(:), INTENT(IN)  :: PZ_O_LMO
   REAL, DIMENSION(SIZE(PZ_O_LMO)) :: BUSINGER_PHIM_1D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_1D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:) < 0. )
     BUSINGER_PHIM_1D(:) = (1.-15.*PZ_O_LMO)**(-0.25)
   ELSEWHERE
     BUSINGER_PHIM_1D(:) = 1. + 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_1D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIM_1D
 !
 !-------------------------------------------------------------------------------
@@ -127,11 +133,14 @@ FUNCTION BUSINGER_PHIM_0D(PZ_O_LMO)
   REAL, INTENT(IN)                   :: PZ_O_LMO
   REAL                               :: BUSINGER_PHIM_0D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_0D',0,ZHOOK_HANDLE)
   IF ( PZ_O_LMO < 0. ) THEN
     BUSINGER_PHIM_0D = (1.-15.*PZ_O_LMO)**(-0.25)
   ELSE
     BUSINGER_PHIM_0D = 1. + 4.7 * PZ_O_LMO
   END IF
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIM_0D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIM_0D
 !
 !-------------------------------------------------------------------------------
@@ -142,11 +151,14 @@ FUNCTION BUSINGER_PHIH_3D(PZ_O_LMO)
   REAL, DIMENSION(SIZE(PZ_O_LMO,1), &
                   SIZE(PZ_O_LMO,2),SIZE(PZ_O_LMO,3)) :: BUSINGER_PHIH_3D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_3D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:,:,:) < 0. )
     BUSINGER_PHIH_3D(:,:,:) = 0.74 * (1.-9.*PZ_O_LMO)**(-0.5)
   ELSEWHERE
     BUSINGER_PHIH_3D(:,:,:) = 0.74 + 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_3D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIH_3D
 !
 !-------------------------------------------------------------------------------
@@ -155,11 +167,14 @@ FUNCTION BUSINGER_PHIH_2D(PZ_O_LMO)
   REAL, DIMENSION(:,:), INTENT(IN)                   :: PZ_O_LMO
   REAL, DIMENSION(SIZE(PZ_O_LMO,1),SIZE(PZ_O_LMO,2)) :: BUSINGER_PHIH_2D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_2D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:,:) < 0. )
     BUSINGER_PHIH_2D(:,:) = 0.74 * (1.-9.*PZ_O_LMO)**(-0.5)
   ELSEWHERE
     BUSINGER_PHIH_2D(:,:) = 0.74 + 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_2D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIH_2D
 !
 !-------------------------------------------------------------------------------
@@ -168,11 +183,14 @@ FUNCTION BUSINGER_PHIH_1D(PZ_O_LMO)
   REAL, DIMENSION(:), INTENT(IN)  :: PZ_O_LMO
   REAL, DIMENSION(SIZE(PZ_O_LMO)) :: BUSINGER_PHIH_1D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_1D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:) < 0. )
     BUSINGER_PHIH_1D(:) = 0.74 * (1.-9.*PZ_O_LMO)**(-0.5)
   ELSEWHERE
     BUSINGER_PHIH_1D(:) = 0.74 + 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_1D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIH_1D
 !
 !-------------------------------------------------------------------------------
@@ -181,11 +199,14 @@ FUNCTION BUSINGER_PHIH_0D(PZ_O_LMO)
   REAL, INTENT(IN)                   :: PZ_O_LMO
   REAL                               :: BUSINGER_PHIH_0D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_0D',0,ZHOOK_HANDLE)
   IF ( PZ_O_LMO < 0. ) THEN
     BUSINGER_PHIH_0D = 0.74 * (1.-9.*PZ_O_LMO)**(-0.5)
   ELSE
     BUSINGER_PHIH_0D = 0.74 + 4.7 * PZ_O_LMO
   END IF
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIH_0D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIH_0D
 !
 !-------------------------------------------------------------------------------
@@ -197,12 +218,15 @@ FUNCTION BUSINGER_PHIE_3D(PZ_O_LMO)
   REAL, DIMENSION(SIZE(PZ_O_LMO,1), &
                   SIZE(PZ_O_LMO,2),SIZE(PZ_O_LMO,3)) :: BUSINGER_PHIE_3D
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIE_3D',0,ZHOOK_HANDLE)
   WHERE ( PZ_O_LMO(:,:,:) < 0. )
     BUSINGER_PHIE_3D(:,:,:) =   (1.+(-PZ_O_LMO)**(2./3.)/XALPSBL) &
                               * (1.-15.*PZ_O_LMO)**(0.5)
   ELSEWHERE
     BUSINGER_PHIE_3D(:,:,:) = 1./(1. + 4.7 * PZ_O_LMO)**2
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:BUSINGER_PHIE_3D',1,ZHOOK_HANDLE)
 END FUNCTION BUSINGER_PHIE_3D
 !
 !-------------------------------------------------------------------------------
@@ -215,6 +239,8 @@ FUNCTION PAULSON_PSIM_2D(PZ_O_LMO)
 !
   REAL, DIMENSION(SIZE(PZ_O_LMO,1),SIZE(PZ_O_LMO,2)) :: ZX
 
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:PAULSON_PSIM_2D',0,ZHOOK_HANDLE)
   ZX=1.
   WHERE ( PZ_O_LMO(:,:) < 0. )
     ZX=(1.-15.*PZ_O_LMO)**(0.25)
@@ -222,6 +248,7 @@ FUNCTION PAULSON_PSIM_2D(PZ_O_LMO)
   ELSEWHERE
     PAULSON_PSIM_2D(:,:) = - 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:PAULSON_PSIM_2D',1,ZHOOK_HANDLE)
 END FUNCTION PAULSON_PSIM_2D
 !
 !-------------------------------------------------------------------------------
@@ -233,6 +260,8 @@ FUNCTION PAULSON_PSIM_1D(PZ_O_LMO)
 !
   REAL, DIMENSION(SIZE(PZ_O_LMO,1)) :: ZX
 
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:PAULSON_PSIM_1D',0,ZHOOK_HANDLE)
   ZX=1.
   WHERE ( PZ_O_LMO(:) < 0. )
     ZX=(1.-15.*PZ_O_LMO)**(0.25)
@@ -240,6 +269,7 @@ FUNCTION PAULSON_PSIM_1D(PZ_O_LMO)
   ELSEWHERE
     PAULSON_PSIM_1D(:) = - 4.7 * PZ_O_LMO
   END WHERE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:PAULSON_PSIM_1D',1,ZHOOK_HANDLE)
 END FUNCTION PAULSON_PSIM_1D
 !
 !-------------------------------------------------------------------------------
@@ -251,6 +281,8 @@ FUNCTION PAULSON_PSIM_0D(PZ_O_LMO)
 !
   REAL                :: ZX
 
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:PAULSON_PSIM_0D',0,ZHOOK_HANDLE)
   ZX=1.
   IF ( PZ_O_LMO < 0. ) THEN
     ZX=(1.-15.*PZ_O_LMO)**(0.25)
@@ -258,6 +290,7 @@ FUNCTION PAULSON_PSIM_0D(PZ_O_LMO)
   ELSE
     PAULSON_PSIM_0D = - 4.7 * PZ_O_LMO
   END IF
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:PAULSON_PSIM_0D',1,ZHOOK_HANDLE)
 END FUNCTION PAULSON_PSIM_0D
 !
 !-------------------------------------------------------------------------------
@@ -278,15 +311,18 @@ FUNCTION LMO_2D(PUSTAR,PTHETA,PRV,PSFTH,PSFRV)
   REAL                                           :: ZEPS
 !
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:LMO_2D',0,ZHOOK_HANDLE)
   ZEPS=(XRV-XRD)/XRD
   ZTHETAV(:,:) = PTHETA(:,:) * ( 1. +ZEPS * PRV(:,:))
-  ZQ0    (:,:) = PSFTH(:,:) + ZTHETAV(:,:) * ZEPS * PSFRV(:,:) 
+  ZQ0    (:,:) = PSFTH(:,:) + ZTHETAV(:,:) * ZEPS * PSFRV(:,:)
 !
   LMO_2D(:,:) = XUNDEF
   WHERE ( ZQ0(:,:) /=0.  )                                   &
     LMO_2D(:,:) = - MAX(PUSTAR(:,:),1.E-6)**3                &
                   / ( XKARMAN * XG / ZTHETAV(:,:) *ZQ0(:,:) )
 
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:LMO_2D',1,ZHOOK_HANDLE)
 END FUNCTION LMO_2D
 !
 !-------------------------------------------------------------------------------
@@ -305,6 +341,8 @@ FUNCTION LMO_1D(PUSTAR,PTHETA,PRV,PSFTH,PSFRV)
   REAL                                           :: ZEPS
 !
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:LMO_1D',0,ZHOOK_HANDLE)
   ZEPS=(XRV-XRD)/XRD
 !
   ZTHETAV(:) = PTHETA(:) * ( 1. +ZEPS * PRV(:))
@@ -314,6 +352,7 @@ FUNCTION LMO_1D(PUSTAR,PTHETA,PRV,PSFTH,PSFRV)
     LMO_1D(:) = - MAX(PUSTAR(:),1.E-6)**3                          &
               / ( XKARMAN * (  XG / ZTHETAV(:)    * PSFTH(:)       &
                              + XG * ZEPS * PSFRV(:) )  )
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:LMO_1D',1,ZHOOK_HANDLE)
 END FUNCTION LMO_1D
 !
 !-------------------------------------------------------------------------------
@@ -332,6 +371,8 @@ FUNCTION LMO_0D(PUSTAR,PTHETA,PRV,PSFTH,PSFRV)
   REAL              :: ZEPS
 !
 !
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:LMO_0D',0,ZHOOK_HANDLE)
   ZEPS=(XRV-XRD)/XRD
 !
 !
@@ -342,6 +383,7 @@ FUNCTION LMO_0D(PUSTAR,PTHETA,PRV,PSFTH,PSFRV)
   LMO_0D = - MAX(PUSTAR,1.E-6)**3                          &
            / ( XKARMAN * (  XG / ZTHETAV       * PSFTH     &
                           + XG * ZEPS * PSFRV )  )
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:LMO_0D',1,ZHOOK_HANDLE)
 END FUNCTION LMO_0D
 !
 !-------------------------------------------------------------------------------
@@ -361,6 +403,8 @@ FUNCTION USTAR_2D(PU,PV,PZ,PZ0,PLMO)
   REAL, DIMENSION(SIZE(PU,1),SIZE(PU,2))         :: ZZ0_O_LMO
 !
 !* purely unstable case
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:USTAR_2D',0,ZHOOK_HANDLE)
   USTAR_2D(:,:) = 0.
   ZZ_O_LMO(:,:) = XUNDEF
   ZZ0_O_LMO(:,:) = XUNDEF
@@ -382,6 +426,7 @@ FUNCTION USTAR_2D(PU,PV,PZ,PZ0,PLMO)
                   * XKARMAN / LOG(PZ(:,:)/PZ0(:,:))
   END WHERE
 !
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:USTAR_2D',1,ZHOOK_HANDLE)
 END FUNCTION USTAR_2D
 !
 !-------------------------------------------------------------------------------
@@ -400,6 +445,8 @@ FUNCTION USTAR_1D(PU,PV,PZ,PZ0,PLMO)
   REAL, DIMENSION(SIZE(PU))                    :: ZZ0_O_LMO
 !
 !* purely unstable case
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:USTAR_1D',0,ZHOOK_HANDLE)
   USTAR_1D(:) = 0.
   ZZ_O_LMO(:) = XUNDEF
   ZZ0_O_LMO(:) = XUNDEF
@@ -421,6 +468,7 @@ FUNCTION USTAR_1D(PU,PV,PZ,PZ0,PLMO)
                   * XKARMAN / LOG(PZ(:)/PZ0(:))
   END WHERE
 !
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:USTAR_1D',1,ZHOOK_HANDLE)
 END FUNCTION USTAR_1D
 !
 !-------------------------------------------------------------------------------
@@ -436,6 +484,8 @@ FUNCTION USTAR_0D(PU,PV,PZ,PZ0,PLMO)
   REAL                           :: USTAR_0D
 !
 !* purely unstable case
+  REAL(KIND=JPRB) :: ZHOOK_HANDLE
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:USTAR_0D',0,ZHOOK_HANDLE)
   USTAR_0D = 0.
 !
 !* general case
@@ -450,6 +500,7 @@ FUNCTION USTAR_0D(PU,PV,PZ,PZ0,PLMO)
   USTAR_0D = SQRT( PU**2+PV**2 )     &
              * XKARMAN / LOG(PZ/PZ0)
 
+  IF (LHOOK) CALL DR_HOOK('MODE_SBL:USTAR_0D',1,ZHOOK_HANDLE)
 END FUNCTION USTAR_0D
 !
 !-------------------------------------------------------------------------------
