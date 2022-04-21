f52c36ce94373b0abb71c76703664daf39a9f234
Sébastien Riette <sebastien.riette@meteo.fr>
2022-04-21 17:14:54 +0200

    Modification of small_3D_alt7, addition of small_3D_alt8
    small_3D_alt7: suppression of the RHCJ option
    small_3D_alt8: RHCJ case
    The RHCJ case cannot be compared to the reference version because
    an incompatible changed has been entered during commit 907e906

--- check_commit_ial.sh
+++ check_commit_ial.sh
@@ -15,6 +15,10 @@ set -e
 #small_3D_alt5: CCLOUD='ICE4'
 #small_3D_alt6: CMF_UPDRAFT='RAHA', CMF_CLOUD='BIGA'
 #small_3D_alt7: CMF_UPDRAFT='RHCJ', CMF_CLOUD='STAT', LOSIGMAS=.FALSE. #Needs 2 corrections in original cycle 48
+#small_3D_alt8: CMF_UPDRAFT='RHCJ'
+
+#The small_3D_alt8 is not included in the list of available tests because it needs to be compared against a special commit.
+#Indeed, on 3 February 2022 (commit 907e906) the mesonh version of compute_updraft_rhcj.F90 has been put in the common directory.
 
 specialPack="ori split recompil"
 availTests="small_3D,small_3D_np2,small_3D_alt1,small_3D_alt2,small_3D_alt3,small_3D_alt4,small_3D_alt5,small_3D_alt6,small_3D_alt7"
