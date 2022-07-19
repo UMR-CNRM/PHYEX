f440e2a24a48d383d5ac6e421932ed78b655d36e
Quentin Rodier <quentin.rodier@meteo.fr>
2022-07-19 16:19:48 +0200

    Quentin 19/07/2022: bugfix GPU merge (duplication of declaration of OTURB_DIAG)

--- src/common/turb/mode_tke_eps_sources.F90
+++ src/common/turb/mode_tke_eps_sources.F90
@@ -182,8 +182,6 @@ CHARACTER(LEN=6),        INTENT(IN)   ::  HPROGRAM     ! CPROGRAM is the program
 TYPE(TFILEDATA),         INTENT(IN)   ::  TPFILE       ! Output file
 LOGICAL,                 INTENT(IN)   ::  OLES_CALL    !
 LOGICAL,                 INTENT(IN)   ::  OTURB_DIAG   ! switch to write some
-LOGICAL,                INTENT(IN)   ::  ODIAG_IN_RUN ! switch to activate online diagnostics (mesonh)
-                                  ! diagnostic fields in the syncronous FM-file
 LOGICAL,                 INTENT(IN)   ::  ODIAG_IN_RUN ! switch to activate online diagnostics (mesonh)
 REAL, DIMENSION(D%NIJT,D%NKT),  INTENT(INOUT)::  PDP          ! Dyn. prod. of TKE
 REAL, DIMENSION(D%NIJT,D%NKT),  INTENT(IN)   ::  PTRH
