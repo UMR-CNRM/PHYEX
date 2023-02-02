1a99ff828ca0a7e34c2be44986dcb9409ec62dd9
Rolf Heilemann Myhre <rolfhm@met.no>
2023-02-02 14:11:30 +0100

    remove where

--- src/common/micro/mode_rain_ice_old_fast_rh.F90
+++ src/common/micro/mode_rain_ice_old_fast_rh.F90
@@ -102,7 +102,7 @@ MODULE MODE_RAIN_ICE_OLD_FAST_RH
     REAL, DIMENSION(KSIZE, KRR) :: ZZW1 ! Work array
 
     INTEGER :: IGWET, IHAIL
-    INTEGER :: JJ
+    INTEGER :: JJ, JK
 
     REAL(KIND=JPRB) :: ZHOOK_HANDLE
 !-------------------------------------------------------------------------------
@@ -112,164 +112,180 @@ MODULE MODE_RAIN_ICE_OLD_FAST_RH
     GHAIL(:) = ZRHT(:)>ICED%XRTMIN(7)
     IHAIL = COUNT(GHAIL(:))
 !
-  IF( IHAIL>0 ) THEN
+    IF( IHAIL>0 ) THEN
 !
 !*       7.2    compute the Wet growth of hail
 !
-    WHERE ( GHAIL(:) )
-      ZLBDAH(:)  = ICED%XLBH*( ZRHODREF(:)*MAX( ZRHT(:),ICED%XRTMIN(7) ) )**ICED%XLBEXH
-    END WHERE
-!
-    ZZW1(:,:) = 0.0
-    WHERE( GHAIL(:) .AND. ((ZRCT(:)>ICED%XRTMIN(2) .AND. ZRCS(:)>0.0)) )
-      ZZW(:) = ZLBDAH(:)**(ICED%XCXH-ICED%XDH-2.0) * ZRHODREF(:)**(-ICED%XCEXVT)
-      ZZW1(:,1) = MIN( ZRCS(:),ICEP%XFWETH * ZRCT(:) * ZZW(:) )             ! RCWETH
-    END WHERE
-    WHERE( GHAIL(:) .AND. ((ZRIT(:)>ICED%XRTMIN(4) .AND. ZRIS(:)>0.0)) )
-      ZZW(:) = ZLBDAH(:)**(ICED%XCXH-ICED%XDH-2.0) * ZRHODREF(:)**(-ICED%XCEXVT)
-      ZZW1(:,2) = MIN( ZRIS(:),ICEP%XFWETH * ZRIT(:) * ZZW(:) )             ! RIWETH
-    END WHERE
+      DO JK = 1, KSIZE
+        IF (GHAIL(JK)) THEN
+          ZLBDAH(JK)  = ICED%XLBH*( ZRHODREF(JK)*MAX( ZRHT(JK),ICED%XRTMIN(7) ) )**ICED%XLBEXH
+        END IF
+      END DO
+
+      ZZW1(:,:) = 0.0
+      DO JK = 1, KSIZE
+        IF (GHAIL(JK) .AND. ((ZRCT(JK)>ICED%XRTMIN(2) .AND. ZRCS(JK)>0.0))) THEN
+          ZZW(JK) = ZLBDAH(JK)**(ICED%XCXH-ICED%XDH-2.0) * ZRHODREF(JK)**(-ICED%XCEXVT)
+          ZZW1(JK,1) = MIN( ZRCS(JK),ICEP%XFWETH * ZRCT(JK) * ZZW(JK) )             ! RCWETH
+        END IF
+      END DO
+      DO JK = 1, KSIZE
+        IF (GHAIL(JK) .AND. ((ZRIT(JK)>ICED%XRTMIN(4) .AND. ZRIS(JK)>0.0))) THEN
+          ZZW(JK) = ZLBDAH(JK)**(ICED%XCXH-ICED%XDH-2.0) * ZRHODREF(JK)**(-ICED%XCEXVT)
+          ZZW1(JK,2) = MIN( ZRIS(JK),ICEP%XFWETH * ZRIT(JK) * ZZW(JK) )             ! RIWETH
+        END IF
+      END DO
 !
 !*       7.2.1  accretion of aggregates on the hailstones
 !
-    GWET(:) = GHAIL(:) .AND. (ZRST(:)>ICED%XRTMIN(5) .AND. ZRSS(:)>0.0)
-    IGWET = COUNT( GWET(:) )
+      GWET(:) = GHAIL(:) .AND. (ZRST(:)>ICED%XRTMIN(5) .AND. ZRSS(:)>0.0)
+      IGWET = COUNT( GWET(:) )
 !
-    IF( IGWET>0 ) THEN
+      IF( IGWET>0 ) THEN
 !
 !*       7.2.3  select the (ZLBDAH,ZLBDAS) couplet
 !
-      ZVEC1(:) = PACK( ZLBDAH(:),MASK=GWET(:) )
-      ZVEC2(:) = PACK( ZLBDAS(:),MASK=GWET(:) )
+        ZVEC1(:) = PACK( ZLBDAH(:),MASK=GWET(:) )
+        ZVEC2(:) = PACK( ZLBDAS(:),MASK=GWET(:) )
 !
 !*       7.2.4  find the next lower indice for the ZLBDAG and for the ZLBDAS
 !               in the geometrical set of (Lbda_h,Lbda_s) couplet use to
 !               tabulate the SWETH-kernel
 !
-      ZVEC1(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAH)-0.00001,           &
-                            ICEP%XWETINTP1H * LOG( ZVEC1(1:IGWET) ) + ICEP%XWETINTP2H ) )
-      IVEC1(1:IGWET) = INT( ZVEC1(1:IGWET) )
-      ZVEC1(1:IGWET) = ZVEC1(1:IGWET) - FLOAT( IVEC1(1:IGWET) )
-!
-      ZVEC2(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAS)-0.00001,           &
-                            ICEP%XWETINTP1S * LOG( ZVEC2(1:IGWET) ) + ICEP%XWETINTP2S ) )
-      IVEC2(1:IGWET) = INT( ZVEC2(1:IGWET) )
-      ZVEC2(1:IGWET) = ZVEC2(1:IGWET) - FLOAT( IVEC2(1:IGWET) )
+        ZVEC1(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAH)-0.00001,           &
+                              ICEP%XWETINTP1H * LOG( ZVEC1(1:IGWET) ) + ICEP%XWETINTP2H ) )
+        IVEC1(1:IGWET) = INT( ZVEC1(1:IGWET) )
+        ZVEC1(1:IGWET) = ZVEC1(1:IGWET) - FLOAT( IVEC1(1:IGWET) )
+
+        ZVEC2(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAS)-0.00001,           &
+                              ICEP%XWETINTP1S * LOG( ZVEC2(1:IGWET) ) + ICEP%XWETINTP2S ) )
+        IVEC2(1:IGWET) = INT( ZVEC2(1:IGWET) )
+        ZVEC2(1:IGWET) = ZVEC2(1:IGWET) - FLOAT( IVEC2(1:IGWET) )
 !
 !*       7.2.5  perform the bilinear interpolation of the normalized
 !               SWETH-kernel
 !
-      DO JJ = 1,IGWET
-        ZVEC3(JJ) = (  ICEP%XKER_SWETH(IVEC1(JJ)+1,IVEC2(JJ)+1)* ZVEC2(JJ)          &
-                     - ICEP%XKER_SWETH(IVEC1(JJ)+1,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
-                                                                   * ZVEC1(JJ) &
-                   - ( ICEP%XKER_SWETH(IVEC1(JJ)  ,IVEC2(JJ)+1)* ZVEC2(JJ)          &
-                     - ICEP%XKER_SWETH(IVEC1(JJ)  ,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
-                                                          * (ZVEC1(JJ) - 1.0)
-      END DO
-      ZZW(:) = UNPACK( VECTOR=ZVEC3(:),MASK=GWET,FIELD=0.0 )
-!
-      WHERE( GWET(:) )
-        ZZW1(:,3) = MIN( ZRSS(:),ICEP%XFSWETH*ZZW(:)                       & ! RSWETH
-                      *( ZLBDAS(:)**(ICED%XCXS-ICED%XBS) )*( ZLBDAH(:)**ICED%XCXH )  &
-                         *( ZRHODREF(:)**(-ICED%XCEXVT-1.) )               &
-                         *( ICEP%XLBSWETH1/( ZLBDAH(:)**2              ) + &
-                            ICEP%XLBSWETH2/( ZLBDAH(:)   * ZLBDAS(:)   ) + &
-                            ICEP%XLBSWETH3/(               ZLBDAS(:)**2) ) )
-      END WHERE
-    END IF
+        DO JJ = 1,IGWET
+          ZVEC3(JJ) = (  ICEP%XKER_SWETH(IVEC1(JJ)+1,IVEC2(JJ)+1)* ZVEC2(JJ)          &
+                       - ICEP%XKER_SWETH(IVEC1(JJ)+1,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
+                                                                 * ZVEC1(JJ) &
+                     - ( ICEP%XKER_SWETH(IVEC1(JJ)  ,IVEC2(JJ)+1)* ZVEC2(JJ)          &
+                       - ICEP%XKER_SWETH(IVEC1(JJ)  ,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
+                                                                * (ZVEC1(JJ) - 1.0)
+        END DO
+        ZZW(:) = UNPACK( VECTOR=ZVEC3(:),MASK=GWET,FIELD=0.0 )
+
+        DO JK = 1, KSIZE
+          IF (GWET(JK)) THEN
+            ZZW1(JK,3) = MIN(ZRSS(JK),ICEP%XFSWETH*ZZW(JK)                       & ! RSWETH
+                          *(ZLBDAS(JK)**(ICED%XCXS-ICED%XBS) )*( ZLBDAH(JK)**ICED%XCXH )  &
+                          *(ZRHODREF(JK)**(-ICED%XCEXVT-1.) )               &
+                          *(ICEP%XLBSWETH1/( ZLBDAH(JK)**2              ) + &
+                            ICEP%XLBSWETH2/( ZLBDAH(JK)   * ZLBDAS(JK)   ) + &
+                            ICEP%XLBSWETH3/(               ZLBDAS(JK)**2)))
+          END IF
+        END DO
+      END IF
 !
 !*       7.2.6  accretion of graupeln on the hailstones
 !
-    GWET(:) = GHAIL(:) .AND. (ZRGT(:)>ICED%XRTMIN(6) .AND. ZRGS(:)>0.0)
-    IGWET = COUNT( GWET(:) )
+      GWET(:) = GHAIL(:) .AND. (ZRGT(:)>ICED%XRTMIN(6) .AND. ZRGS(:)>0.0)
+      IGWET = COUNT( GWET(:) )
 !
-    IF( IGWET>0 ) THEN
+      IF( IGWET>0 ) THEN
 !
 !*       7.2.8  select the (ZLBDAH,ZLBDAG) couplet
 !
-      ZVEC1(:) = PACK( ZLBDAH(:),MASK=GWET(:) )
-      ZVEC2(:) = PACK( ZLBDAG(:),MASK=GWET(:) )
+        ZVEC1(:) = PACK( ZLBDAH(:),MASK=GWET(:) )
+        ZVEC2(:) = PACK( ZLBDAG(:),MASK=GWET(:) )
 !
 !*       7.2.9  find the next lower indice for the ZLBDAH and for the ZLBDAG
 !               in the geometrical set of (Lbda_h,Lbda_g) couplet use to
 !               tabulate the GWETH-kernel
 !
-      ZVEC1(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAG)-0.00001,           &
-                            ICEP%XWETINTP1H * LOG( ZVEC1(1:IGWET) ) + ICEP%XWETINTP2H ) )
-      IVEC1(1:IGWET) = INT( ZVEC1(1:IGWET) )
-      ZVEC1(1:IGWET) = ZVEC1(1:IGWET) - FLOAT( IVEC1(1:IGWET) )
-!
-      ZVEC2(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAG)-0.00001,           &
-                            ICEP%XWETINTP1G * LOG( ZVEC2(1:IGWET) ) + ICEP%XWETINTP2G ) )
-      IVEC2(1:IGWET) = INT( ZVEC2(1:IGWET) )
-      ZVEC2(1:IGWET) = ZVEC2(1:IGWET) - FLOAT( IVEC2(1:IGWET) )
+        ZVEC1(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAG)-0.00001,           &
+                              ICEP%XWETINTP1H * LOG( ZVEC1(1:IGWET) ) + ICEP%XWETINTP2H ) )
+        IVEC1(1:IGWET) = INT( ZVEC1(1:IGWET) )
+        ZVEC1(1:IGWET) = ZVEC1(1:IGWET) - FLOAT( IVEC1(1:IGWET) )
+
+        ZVEC2(1:IGWET) = MAX( 1.00001, MIN( FLOAT(ICEP%NWETLBDAG)-0.00001,           &
+                              ICEP%XWETINTP1G * LOG( ZVEC2(1:IGWET) ) + ICEP%XWETINTP2G ) )
+        IVEC2(1:IGWET) = INT( ZVEC2(1:IGWET) )
+        ZVEC2(1:IGWET) = ZVEC2(1:IGWET) - FLOAT( IVEC2(1:IGWET) )
 !
 !*       7.2.10 perform the bilinear interpolation of the normalized
 !               GWETH-kernel
 !
-      DO JJ = 1,IGWET
-        ZVEC3(JJ) = (  ICEP%XKER_GWETH(IVEC1(JJ)+1,IVEC2(JJ)+1)* ZVEC2(JJ)          &
-                     - ICEP%XKER_GWETH(IVEC1(JJ)+1,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
-                                                                   * ZVEC1(JJ) &
-                  - (  ICEP%XKER_GWETH(IVEC1(JJ)  ,IVEC2(JJ)+1)* ZVEC2(JJ)          &
-                     - ICEP%XKER_GWETH(IVEC1(JJ)  ,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
-                                                          * (ZVEC1(JJ) - 1.0)
-      END DO
-      ZZW(:) = UNPACK( VECTOR=ZVEC3(:),MASK=GWET,FIELD=0.0 )
-!
-      WHERE( GWET(:) )
-        ZZW1(:,5) = MAX(MIN( ZRGS(:),ICEP%XFGWETH*ZZW(:)                       & ! RGWETH
-                      *( ZLBDAG(:)**(ICED%XCXG-ICED%XBG) )*( ZLBDAH(:)**ICED%XCXH )  &
-                         *( ZRHODREF(:)**(-ICED%XCEXVT-1.) )               &
-                         *( ICEP%XLBGWETH1/( ZLBDAH(:)**2              ) + &
-                            ICEP%XLBGWETH2/( ZLBDAH(:)   * ZLBDAG(:)   ) + &
-                            ICEP%XLBGWETH3/(               ZLBDAG(:)**2) ) ),0. )
-      END WHERE
-    END IF
+        DO JJ = 1,IGWET
+          ZVEC3(JJ) = (  ICEP%XKER_GWETH(IVEC1(JJ)+1,IVEC2(JJ)+1)* ZVEC2(JJ)          &
+                       - ICEP%XKER_GWETH(IVEC1(JJ)+1,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
+                                                                 * ZVEC1(JJ) &
+                    - (  ICEP%XKER_GWETH(IVEC1(JJ)  ,IVEC2(JJ)+1)* ZVEC2(JJ)          &
+                       - ICEP%XKER_GWETH(IVEC1(JJ)  ,IVEC2(JJ)  )*(ZVEC2(JJ) - 1.0) ) &
+                                                                * (ZVEC1(JJ) - 1.0)
+        END DO
+        ZZW(:) = UNPACK( VECTOR=ZVEC3(:),MASK=GWET,FIELD=0.0 )
+
+        DO JK = 1, KSIZE
+          IF (GWET(JK)) THEN
+            ZZW1(JK,5) = MAX(MIN( ZRGS(JK),ICEP%XFGWETH*ZZW(JK)                       & ! RGWETH
+                          *( ZLBDAG(JK)**(ICED%XCXG-ICED%XBG) )*( ZLBDAH(JK)**ICED%XCXH )  &
+                             *( ZRHODREF(JK)**(-ICED%XCEXVT-1.) )               &
+                             *( ICEP%XLBGWETH1/( ZLBDAH(JK)**2              ) + &
+                                ICEP%XLBGWETH2/( ZLBDAH(JK)   * ZLBDAG(JK)   ) + &
+                                ICEP%XLBGWETH3/(               ZLBDAG(JK)**2) ) ),0. )
+          END IF
+        END DO
+      END IF
 !
 !*       7.3    compute the Wet growth of hail
 !
-    ZZW(:) = 0.0
-    WHERE( GHAIL(:) .AND. ZZT(:)<CST%XTT )
-      ZZW(:) = ZRVT(:)*ZPRES(:)/(CST%XEPSILO+ZRVT(:)) ! Vapor pressure
-      ZZW(:) = ZKA(:)*(CST%XTT-ZZT(:)) +                                 &
-                ( ZDV(:)*(CST%XLVTT + ( CST%XCPV - CST%XCL ) * ( ZZT(:) - CST%XTT )) &
-                            *(CST%XESTT-ZZW(:))/(CST%XRV*ZZT(:)))
+      ZZW(:) = 0.0
+      DO JK = 1, KSIZE
+        IF (GHAIL(JK) .AND. ZZT(JK)<CST%XTT) THEN
+          ZZW(JK) = ZRVT(JK)*ZPRES(JK)/(CST%XEPSILO+ZRVT(JK)) ! Vapor pressure
+          ZZW(JK) = ZKA(JK)*(CST%XTT-ZZT(JK)) +                                 &
+                    ( ZDV(JK)*(CST%XLVTT + ( CST%XCPV - CST%XCL ) * ( ZZT(JK) - CST%XTT )) &
+                                *(CST%XESTT-ZZW(JK))/(CST%XRV*ZZT(JK)))
 !
-! compute RWETH
+!         compute RWETH
 !
-      ZZW(:)  =  MAX(0.,  ( ZZW(:) * ( ICEP%X0DEPH*       ZLBDAH(:)**ICEP%XEX0DEPH + &
-                                ICEP%X1DEPH*ZCJ(:)*ZLBDAH(:)**ICEP%XEX1DEPH ) + &
-                   ( ZZW1(:,2)+ZZW1(:,3)+ZZW1(:,5) ) *                  &
-                   ( ZRHODREF(:)*(CST%XLMTT+(CST%XCI-CST%XCL)*(CST%XTT-ZZT(:))))) / &
-                         ( ZRHODREF(:)*(CST%XLMTT-CST%XCL*(CST%XTT-ZZT(:))) ) )
+          ZZW(JK)  =  MAX(0.,  ( ZZW(JK) * ( ICEP%X0DEPH*       ZLBDAH(JK)**ICEP%XEX0DEPH + &
+                                    ICEP%X1DEPH*ZCJ(JK)*ZLBDAH(JK)**ICEP%XEX1DEPH ) + &
+                       ( ZZW1(JK,2)+ZZW1(JK,3)+ZZW1(JK,5) ) *                  &
+                       ( ZRHODREF(JK)*(CST%XLMTT+(CST%XCI-CST%XCL)*(CST%XTT-ZZT(JK))))) / &
+                             ( ZRHODREF(JK)*(CST%XLMTT-CST%XCL*(CST%XTT-ZZT(JK))) ) )
 !
-      ZZW1(:,6) = MAX( ZZW(:) - ZZW1(:,2) - ZZW1(:,3) - ZZW1(:,5),0.) ! RCWETH+RRWETH
-    END WHERE
-    WHERE ( GHAIL(:) .AND. ZZT(:)<CST%XTT  .AND. ZZW1(:,6)/=0.)
+          ZZW1(JK,6) = MAX(ZZW(JK) - ZZW1(JK,2) - ZZW1(JK,3) - ZZW1(JK,5), 0.) ! RCWETH+RRWETH
+        END IF
+      END DO
+
+      DO JK = 1, KSIZE
+        IF (GHAIL(JK) .AND. ZZT(JK)<CST%XTT  .AND. ZZW1(JK,6)/=0.) THEN
 !
 ! limitation of the available rainwater mixing ratio (RRWETH < RRS !)
 !
-      ZZW1(:,4) = MAX( 0.0,MIN( ZZW1(:,6),ZRRS(:)+ZZW1(:,1) ) )
-      ZUSW(:)   = ZZW1(:,4) / ZZW1(:,6)
-      ZZW1(:,2) = ZZW1(:,2)*ZUSW(:)
-      ZZW1(:,3) = ZZW1(:,3)*ZUSW(:)
-      ZZW1(:,5) = ZZW1(:,5)*ZUSW(:)
-      ZZW(:)    = ZZW1(:,4) + ZZW1(:,2) + ZZW1(:,3) + ZZW1(:,5)
+          ZZW1(JK,4) = MAX( 0.0,MIN( ZZW1(JK,6),ZRRS(JK)+ZZW1(JK,1) ) )
+          ZUSW(JK)   = ZZW1(JK,4) / ZZW1(JK,6)
+          ZZW1(JK,2) = ZZW1(JK,2)*ZUSW(JK)
+          ZZW1(JK,3) = ZZW1(JK,3)*ZUSW(JK)
+          ZZW1(JK,5) = ZZW1(JK,5)*ZUSW(JK)
+          ZZW(JK)    = ZZW1(JK,4) + ZZW1(JK,2) + ZZW1(JK,3) + ZZW1(JK,5)
 !
 !*       7.1.6  integrate the Wet growth of hail
 !
-      ZRCS(:) = ZRCS(:) - ZZW1(:,1)
-      ZRIS(:) = ZRIS(:) - ZZW1(:,2)
-      ZRSS(:) = ZRSS(:) - ZZW1(:,3)
-      ZRGS(:) = ZRGS(:) - ZZW1(:,5)
-      ZRHS(:) = ZRHS(:) + ZZW(:)
-      ZRRS(:) = MAX( 0.0,ZRRS(:) - ZZW1(:,4) + ZZW1(:,1) )
-      ZTHS(:) = ZTHS(:) + ZZW1(:,4)*(ZLSFACT(:)-ZLVFACT(:))
+          ZRCS(JK) = ZRCS(JK) - ZZW1(JK,1)
+          ZRIS(JK) = ZRIS(JK) - ZZW1(JK,2)
+          ZRSS(JK) = ZRSS(JK) - ZZW1(JK,3)
+          ZRGS(JK) = ZRGS(JK) - ZZW1(JK,5)
+          ZRHS(JK) = ZRHS(JK) + ZZW(JK)
+          ZRRS(JK) = MAX( 0.0,ZRRS(JK) - ZZW1(JK,4) + ZZW1(JK,1) )
+          ZTHS(JK) = ZTHS(JK) + ZZW1(JK,4)*(ZLSFACT(JK)-ZLVFACT(JK))
                            ! f(L_f*(RCWETH+RRWETH))
-    END WHERE
-  END IF
+        END IF
+      END DO
+    END IF
+
     IF (LBUDGET_TH) CALL BUDGET_DDH(UNPACK(ZTHS(:),MASK=GMICRO(:,:),FIELD=PTHS)*PRHODJ(:,:), &
                                      4,'WETH_BU_RTH',YDDDH, YDLDDH, YDMDDH)
     IF (LBUDGET_RC) CALL BUDGET_DDH(UNPACK(ZRCS(:)*ZRHODJ(:),MASK=GMICRO(:,:),FIELD=0.0),    &
@@ -287,38 +303,41 @@ MODULE MODE_RAIN_ICE_OLD_FAST_RH
 
 !*       7.45   Conversion of the hailstones into graupel
 
-  IF( IHAIL>0 ) THEN
+    IF( IHAIL>0 ) THEN
 !
 !*       7.5    Melting of the hailstones
 !
-    ZZW(:) = 0.0
-    WHERE( GHAIL(:) .AND. (ZRHS(:)>0.0) .AND. (ZZT(:)>CST%XTT) )
-      ZZW(:) = ZRVT(:)*ZPRES(:)/(CST%XEPSILO+ZRVT(:)) ! Vapor pressure
-      ZZW(:) = ZKA(:)*(CST%XTT-ZZT(:)) +                              &
-             ( ZDV(:)*(CST%XLVTT + ( CST%XCPV - CST%XCL ) * ( ZZT(:) - CST%XTT )) &
-                             *(CST%XESTT-ZZW(:))/(CST%XRV*ZZT(:)))
+      ZZW(:) = 0.0
+      DO JK = 1, KSIZE
+        IF (GHAIL(JK) .AND. (ZRHS(JK)>0.0) .AND. (ZZT(JK)>CST%XTT)) THEN
+          ZZW(JK) = ZRVT(JK)*ZPRES(JK)/(CST%XEPSILO+ZRVT(JK)) ! Vapor pressure
+          ZZW(JK) = ZKA(JK)*(CST%XTT-ZZT(JK)) +                              &
+                 ( ZDV(JK)*(CST%XLVTT + ( CST%XCPV - CST%XCL ) * ( ZZT(JK) - CST%XTT )) &
+                                 *(CST%XESTT-ZZW(JK))/(CST%XRV*ZZT(JK)))
 !
 ! compute RHMLTR
 !
-      ZZW(:)  = MIN( ZRHS(:), MAX( 0.0,( -ZZW(:) *                     &
-                             ( ICEP%X0DEPH*       ZLBDAH(:)**ICEP%XEX0DEPH +     &
-                               ICEP%X1DEPH*ZCJ(:)*ZLBDAH(:)**ICEP%XEX1DEPH ) -   &
-                      ZZW1(:,6)*( ZRHODREF(:)*CST%XCL*(CST%XTT-ZZT(:))) ) /    &
-                                               ( ZRHODREF(:)*CST%XLMTT)))
-      ZRRS(:) = ZRRS(:) + ZZW(:)
-      ZRHS(:) = ZRHS(:) - ZZW(:)
-      ZTHS(:) = ZTHS(:) - ZZW(:)*(ZLSFACT(:)-ZLVFACT(:)) ! f(L_f*(-RHMLTR))
-    END WHERE
-  END IF
+          ZZW(JK)  = MIN( ZRHS(JK), MAX( 0.0,( -ZZW(JK) *                     &
+                                 ( ICEP%X0DEPH*       ZLBDAH(JK)**ICEP%XEX0DEPH +     &
+                                   ICEP%X1DEPH*ZCJ(JK)*ZLBDAH(JK)**ICEP%XEX1DEPH ) -   &
+                          ZZW1(JK,6)*( ZRHODREF(JK)*CST%XCL*(CST%XTT-ZZT(JK))) ) /    &
+                                                   ( ZRHODREF(JK)*CST%XLMTT)))
+          ZRRS(JK) = ZRRS(JK) + ZZW(JK)
+          ZRHS(JK) = ZRHS(JK) - ZZW(JK)
+          ZTHS(JK) = ZTHS(JK) - ZZW(JK)*(ZLSFACT(JK)-ZLVFACT(JK)) ! f(L_f*(-RHMLTR))
+        END IF
+      END DO
+    END IF
 
-  IF (LBUDGET_TH) CALL BUDGET_DDH(UNPACK(ZTHS(:),MASK=GMICRO(:,:),FIELD=PTHS)*PRHODJ(:,:),&
-                                   4,'HMLT_BU_RTH',YDDDH, YDLDDH, YDMDDH)
-  IF (LBUDGET_RR) CALL BUDGET_DDH(UNPACK(ZRRS(:)*ZRHODJ(:),MASK=GMICRO(:,:),FIELD=0.0), &
-                                   8,'HMLT_BU_RRR',YDDDH, YDLDDH, YDMDDH)
-  IF (LBUDGET_RH) CALL BUDGET_DDH(UNPACK(ZRHS(:)*ZRHODJ(:),MASK=GMICRO(:,:),FIELD=0.0), &
-                                  12,'HMLT_BU_RRH',YDDDH, YDLDDH, YDMDDH)
+    IF (LBUDGET_TH) CALL BUDGET_DDH(UNPACK(ZTHS(:),MASK=GMICRO(:,:),FIELD=PTHS)*PRHODJ(:,:),&
+                                     4,'HMLT_BU_RTH',YDDDH, YDLDDH, YDMDDH)
+    IF (LBUDGET_RR) CALL BUDGET_DDH(UNPACK(ZRRS(:)*ZRHODJ(:),MASK=GMICRO(:,:),FIELD=0.0), &
+                                     8,'HMLT_BU_RRR',YDDDH, YDLDDH, YDMDDH)
+    IF (LBUDGET_RH) CALL BUDGET_DDH(UNPACK(ZRHS(:)*ZRHODJ(:),MASK=GMICRO(:,:),FIELD=0.0), &
+                                    12,'HMLT_BU_RRH',YDDDH, YDLDDH, YDMDDH)
 
-  IF (LHOOK) CALL DR_HOOK('RAIN_ICE_OLD:RAIN_ICE_FAST_RH',1,ZHOOK_HANDLE)
+    IF (LHOOK) CALL DR_HOOK('RAIN_ICE_OLD:RAIN_ICE_FAST_RH',1,ZHOOK_HANDLE)
+
   END SUBROUTINE RAIN_ICE_OLD_FAST_RH
 
 END MODULE MODE_RAIN_ICE_OLD_FAST_RH
