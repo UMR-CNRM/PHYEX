7b0a97d7f32f7f7b536ede527673ff85755e9318
VIE Benoit <vie@sxphynh>
2023-02-20 15:56:57 +0100

    bugfixes

--- src/arome/micro/mode_set_conc_lima_lbc.F90
+++ src/arome/micro/mode_set_conc_lima_lbc.F90
@@ -78,7 +78,9 @@ USE MODD_PARAM_LIMA,      ONLY : XRTMIN, XCTMIN, NMOD_CCN, NMOD_IFN, &
 USE MODD_PARAM_LIMA_COLD, ONLY : XAI, XBI, XAS, XBS
 USE MODD_PARAM_LIMA_MIXED,ONLY : XAG, XBG, XAH, XBH
 USE MODD_NSV,             ONLY : NSV_LIMA_BEG_A, NSV_LIMA_NC_A, NSV_LIMA_NR_A, NSV_LIMA_CCN_ACTI_A, &
-                                 NSV_LIMA_NI_A, NSV_LIMA_NS_A, NSV_LIMA_NG_A, NSV_LIMA_NH_A, NSV_LIMA_IFN_NUCL_A
+                                 NSV_LIMA_NI_A, NSV_LIMA_NS_A, NSV_LIMA_NG_A, NSV_LIMA_NH_A, NSV_LIMA_IFN_NUCL_A, &
+                                 NSV_LIMA_BEG, NSV_LIMA_NC, NSV_LIMA_NR, NSV_LIMA_CCN_ACTI, &
+                                 NSV_LIMA_NI, NSV_LIMA_NS, NSV_LIMA_NG, NSV_LIMA_NH, NSV_LIMA_IFN_NUCL
 USE MODD_CST,             ONLY : XPI, XRHOLW, XRHOLI
 USE MODD_CONF,            ONLY : NVERB
 !
@@ -92,17 +94,27 @@ REAL, DIMENSION(:,:,:),    INTENT(IN) :: PRHODREF   ! Reference density
 !
 REAL, DIMENSION(:,:,:,:),  INTENT(INOUT) :: PRT     ! microphysical mixing ratios
 !
-REAL,  DIMENSION(:,:,:,NSV_LIMA_BEG_A(kmi):), INTENT(INOUT):: PSVT     ! microphys. concentrations
+REAL,  DIMENSION(:,:,:,:), INTENT(INOUT) :: PSVT     ! microphys. concentrations
 !
 !
 !*       0.2   Declarations of local variables :
 !
 REAL       :: ZCONC
+INTEGER    :: ISV_LIMA_NC, ISV_LIMA_NR, ISV_LIMA_CCN_ACTI
+INTEGER    :: ISV_LIMA_NI, ISV_LIMA_NS, ISV_LIMA_NG, ISV_LIMA_NH, ISV_LIMA_IFN_NUCL
 !
 !-------------------------------------------------------------------------------
 !*       1.    RETRIEVE LOGICAL UNIT NUMBER
 !              ----------------------------
 !
+ISV_LIMA_NC       = NSV_LIMA_NC       - NSV_LIMA_BEG + 1
+ISV_LIMA_NR       = NSV_LIMA_NR       - NSV_LIMA_BEG + 1
+ISV_LIMA_CCN_ACTI = NSV_LIMA_CCN_ACTI - NSV_LIMA_BEG + 1
+ISV_LIMA_NI       = NSV_LIMA_NI       - NSV_LIMA_BEG + 1
+ISV_LIMA_NS       = NSV_LIMA_NS       - NSV_LIMA_BEG + 1
+ISV_LIMA_NG       = NSV_LIMA_NG       - NSV_LIMA_BEG + 1
+ISV_LIMA_NH       = NSV_LIMA_NH       - NSV_LIMA_BEG + 1
+ISV_LIMA_IFN_NUCL = NSV_LIMA_IFN_NUCL - NSV_LIMA_BEG + 1
 !
 !*       2.    INITIALIZATION
 !              --------------
@@ -112,20 +124,20 @@ IF (NMOM_C.GE.2) THEN
 !  droplets
 !
    ZCONC = 300.E6 ! droplet concentration set at 300 cm-3
-   WHERE ( PRT(:,:,:,2) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NC_A(kmi))<1.E-11)
-      PSVT(:,:,:,NSV_LIMA_NC_A(kmi)) = ZCONC
+   WHERE ( PRT(:,:,:,2) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NC)<1.E-11)
+      PSVT(:,:,:,ISV_LIMA_NC) = ZCONC
    END WHERE
-   WHERE ( PRT(:,:,:,2) <= 1.E-11  .AND. PSVT(:,:,:,NSV_LIMA_NC_A(kmi))<1.E-11)
+   WHERE ( PRT(:,:,:,2) <= 1.E-11  .AND. PSVT(:,:,:,ISV_LIMA_NC)<1.E-11)
       PRT(:,:,:,2)  = 0.0
-      PSVT(:,:,:,NSV_LIMA_NC_A(kmi)) = 0.0
+      PSVT(:,:,:,ISV_LIMA_NC) = 0.0
    END WHERE
    
    IF (NMOD_CCN .GE. 1) THEN
-      WHERE ( PRT(:,:,:,2) > 1.E-11  .AND. PSVT(:,:,:,NSV_LIMA_NC_A(kmi))<1.E-11)
-         PSVT(:,:,:,NSV_LIMA_CCN_ACTI_A(kmi)) = ZCONC
+      WHERE ( PRT(:,:,:,2) > 1.E-11  .AND. PSVT(:,:,:,ISV_LIMA_NC)<1.E-11)
+         PSVT(:,:,:,ISV_LIMA_CCN_ACTI) = ZCONC
       END WHERE
-      WHERE ( PRT(:,:,:,2) <= 1.E-11  .AND. PSVT(:,:,:,NSV_LIMA_NC_A(kmi))<1.E-11)
-         PSVT(:,:,:,NSV_LIMA_CCN_ACTI_A(kmi)) = 0.0
+      WHERE ( PRT(:,:,:,2) <= 1.E-11  .AND. PSVT(:,:,:,ISV_LIMA_NC)<1.E-11)
+         PSVT(:,:,:,ISV_LIMA_CCN_ACTI) = 0.0
       END WHERE
    END IF
    
@@ -137,15 +149,15 @@ IF (NMOM_R.GE.2) THEN
 !
    ZCONC = (1.E7)**3/(XPI*XRHOLW) ! cf XCONCR_PARAM_INI in ini_rain_c2r2.f90
    IF (HGETCLOUD == 'INI1') THEN ! init from REVE scheme
-      PSVT(:,:,:,NSV_LIMA_NR_A(kmi)) = 0.0
+      PSVT(:,:,:,ISV_LIMA_NR) = 0.0
    ELSE ! init from KESS, ICE3...
-      WHERE ( PRT(:,:,:,3) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NR_A(kmi))<1.E-11 )
-         PSVT(:,:,:,NSV_LIMA_NR_A(kmi)) = MAX( SQRT(SQRT(PRHODREF(:,:,:)*PRT(:,:,:,3) &
+      WHERE ( PRT(:,:,:,3) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NR)<1.E-11 )
+         PSVT(:,:,:,ISV_LIMA_NR) = MAX( SQRT(SQRT(PRHODREF(:,:,:)*PRT(:,:,:,3) &
               *ZCONC)),1. )
       END WHERE
-      WHERE ( PRT(:,:,:,3) <= 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NR_A(kmi))<1.E-11 )
+      WHERE ( PRT(:,:,:,3) <= 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NR)<1.E-11 )
          PRT(:,:,:,3)  = 0.0
-         PSVT(:,:,:,NSV_LIMA_NR_A(kmi)) = 0.0
+         PSVT(:,:,:,ISV_LIMA_NR) = 0.0
       END WHERE
    END IF
 END IF
@@ -155,69 +167,69 @@ IF (NMOM_I.GE.2) THEN
 ! ice crystals
 !
    ZCONC = 100.E3 ! maximum ice concentration set at 100/L
-   WHERE ( PRT(:,:,:,4) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NI_A(kmi))<1.E-11 )
+   WHERE ( PRT(:,:,:,4) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NI)<1.E-11 )
 !
 !      PSVT(:,:,:,NSV_LIMA_NI_A(kmi)) = MIN( PRHODREF(:,:,:) /                                     &
 !           ( XRHOLI * XAI*(10.E-06)**XBI * PRT(:,:,:,4) ), &
 !           ZCONC )
 ! Correction
-      PSVT(:,:,:,NSV_LIMA_NI_A(kmi)) = MIN(PRT(:,:,:,4)/(0.82*(10.E-06)**2.5),ZCONC )
+      PSVT(:,:,:,ISV_LIMA_NI) = MIN(PRT(:,:,:,4)/(0.82*(10.E-06)**2.5),ZCONC )
    END WHERE
-   WHERE ( PRT(:,:,:,4) <= 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NI_A(kmi))<1.E-11 )
+   WHERE ( PRT(:,:,:,4) <= 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NI)<1.E-11 )
       PRT(:,:,:,4)  = 0.0
-      PSVT(:,:,:,NSV_LIMA_NI_A(kmi)) = 0.0
+      PSVT(:,:,:,ISV_LIMA_NI) = 0.0
    END WHERE
 
    IF (NMOD_IFN .GE. 1) THEN
-      WHERE ( PRT(:,:,:,4) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NI_A(kmi))<1.E-11 )
-         PSVT(:,:,:,NSV_LIMA_IFN_NUCL_A(kmi)) = PSVT(:,:,:,NSV_LIMA_NI_A(kmi))
+      WHERE ( PRT(:,:,:,4) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NI)<1.E-11 )
+         PSVT(:,:,:,ISV_LIMA_IFN_NUCL) = PSVT(:,:,:,ISV_LIMA_NI)
       END WHERE
-      WHERE ( PRT(:,:,:,4) <= 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NI_A(kmi))<1.E-11 )
-         PSVT(:,:,:,NSV_LIMA_IFN_NUCL_A(kmi)) = 0.0
+      WHERE ( PRT(:,:,:,4) <= 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NI)<1.E-11 )
+         PSVT(:,:,:,ISV_LIMA_IFN_NUCL) = 0.0
       END WHERE
    END IF
 
 END IF
 !
-IF (NSV_LIMA_NS_A(KMI).GE.1) THEN
+IF (ISV_LIMA_NS.GE.1) THEN
 !
 !  snow
 !
    ZCONC = 1./ (XAS*0.001**XBS) ! 1mm particle size
-   WHERE ( PRT(:,:,:,5) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NS_A(kmi))<1.E-11 )
-      PSVT(:,:,:,NSV_LIMA_NS_A(KMI)) = PRT(:,:,:,5) * ZCONC
+   WHERE ( PRT(:,:,:,5) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NS)<1.E-11 )
+      PSVT(:,:,:,ISV_LIMA_NS) = PRT(:,:,:,5) * ZCONC
    END WHERE
-   WHERE ( PRT(:,:,:,5) <= 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NS_A(kmi))<1.E-11 )
+   WHERE ( PRT(:,:,:,5) <= 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NS)<1.E-11 )
       PRT(:,:,:,5)  = 0.0
-      PSVT(:,:,:,NSV_LIMA_NS_A(KMI)) = 0.0
+      PSVT(:,:,:,ISV_LIMA_NS) = 0.0
    END WHERE
 END IF
 !
-IF (NSV_LIMA_NG_A(KMI).GE.1) THEN
+IF (ISV_LIMA_NG.GE.1) THEN
 !
 !  graupel
 !
    ZCONC = 1./ (XAG*0.001**XBG) ! 1mm particle size
-   WHERE ( PRT(:,:,:,6) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NG_A(kmi))<1.E-11 )
-      PSVT(:,:,:,NSV_LIMA_NG_A(KMI)) = PRT(:,:,:,6) * ZCONC
+   WHERE ( PRT(:,:,:,6) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NG)<1.E-11 )
+      PSVT(:,:,:,ISV_LIMA_NG) = PRT(:,:,:,6) * ZCONC
    END WHERE
-   WHERE ( PRT(:,:,:,6) <= 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NG_A(kmi))<1.E-11 )
+   WHERE ( PRT(:,:,:,6) <= 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NG)<1.E-11 )
       PRT(:,:,:,6)  = 0.0
-      PSVT(:,:,:,NSV_LIMA_NG_A(KMI)) = 0.0
+      PSVT(:,:,:,ISV_LIMA_NG) = 0.0
    END WHERE
 END IF
 !
-IF (NSV_LIMA_NH_A(KMI).GE.1) THEN
+IF (ISV_LIMA_NH.GE.1) THEN
 !
 !  hail
 !
    ZCONC = 1./ (XAH*0.001**XBH) ! 1mm particle size
-   WHERE ( PRT(:,:,:,7) > 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NH_A(kmi))<1.E-11 )
-      PSVT(:,:,:,NSV_LIMA_NH_A(KMI)) = PRT(:,:,:,7) * ZCONC
+   WHERE ( PRT(:,:,:,7) > 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NH)<1.E-11 )
+      PSVT(:,:,:,ISV_LIMA_NH) = PRT(:,:,:,7) * ZCONC
    END WHERE
-   WHERE ( PRT(:,:,:,7) <= 1.E-11 .AND. PSVT(:,:,:,NSV_LIMA_NH_A(kmi))<1.E-11 )
+   WHERE ( PRT(:,:,:,7) <= 1.E-11 .AND. PSVT(:,:,:,ISV_LIMA_NH)<1.E-11 )
       PRT(:,:,:,7)  = 0.0
-      PSVT(:,:,:,NSV_LIMA_NH_A(KMI)) = 0.0
+      PSVT(:,:,:,ISV_LIMA_NH) = 0.0
    END WHERE
 END IF
 !
