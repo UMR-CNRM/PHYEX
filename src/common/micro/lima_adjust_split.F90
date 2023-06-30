901d692eaa5476c50a48f71261db8505c7b88d8b
Quentin Rodier <quentin.rodier@meteo.fr>
2023-06-30 15:47:31 +0200

    comment CALL CONDENSATION in lima_adjust_split because not transformed and PSIGQSAT is now real

--- src/common/micro/lima_adjust_split.F90
+++ src/common/micro/lima_adjust_split.F90
@@ -460,13 +460,13 @@ DO JITER =1,ITERMAX
    END IF
 
    IF (LADJ) THEN
-      CALL CONDENSATION(D, CST, RAIN_ICE_PARAMN, NEBN, TURBN,                    &
-          'S', HCONDENS, HLAMBDA3,                                             &
-           PPABST, PZZ, PRHODREF, ZT, ZRV_IN, ZRV, ZRC_IN, ZRC, ZRI_IN, ZRI,   &
-           PRRS*PTSTEP,PRSS*PTSTEP, PRGS*PTSTEP, &
-           Z_SIGS, .FALSE., PMFCONV, PCLDFR, Z_SRCS, GUSERI, G_SIGMAS, .FALSE., &
-           ZDUM, ZDUM, ZDUM, ZDUM, ZDUM,              &
-           ZSIGQSAT2D, PLV=ZLV, PLS=ZLS, PCPH=ZCPH )
+      !CALL CONDENSATION(D, CST, RAIN_ICE_PARAMN, NEBN, TURBN,                    &
+      !    'S', HCONDENS, HLAMBDA3,                                             &
+      !     PPABST, PZZ, PRHODREF, ZT, ZRV_IN, ZRV, ZRC_IN, ZRC, ZRI_IN, ZRI,   &
+      !     PRRS*PTSTEP,PRSS*PTSTEP, PRGS*PTSTEP, &
+      !     Z_SIGS, .FALSE., PMFCONV, PCLDFR, Z_SRCS, GUSERI, G_SIGMAS, .FALSE., &
+      !     ZDUM, ZDUM, ZDUM, ZDUM, ZDUM,              &
+      !     ZSIGQSAT2D, PLV=ZLV, PLS=ZLS, PCPH=ZCPH )
    END IF
    IF (OSUBG_COND .AND. NMOM_C.GE.2 .AND. LACTI) THEN
       PSRCS=Z_SRCS
