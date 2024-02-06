ca18ef2bf51d3793831aa6d228bc95a650c3771c
Sébastien Riette <sebastien.riette@meteo.fr>
2024-02-06 08:06:27 +0100

    S. Riette 6 Feb 2024: another testing BF

--- tools/testing.sh
+++ tools/testing.sh
@@ -272,14 +272,14 @@ if [ ${force} -eq 1 -o $(get_statuses "${SHA}" | grep "${context}" | wc -l) -eq 
       git checkout "${SHA}"
       cd "${currentdir}"
     fi
-    log 1 "Installing/updating PHYEX"
-    ${WORKDIR}/PHYEX/tools/INSTALL.sh --ALL
     if [ -f "${WORKDIR}/PHYEX/tools/testing.sh" ]; then
       if [ "${currentMD5}" != $(md5sum "${WORKDIR}/PHYEX/tools/testing.sh" | cut -d\  -f1) ]; then
         log 1 "Script has changed, running the new version" #This log and the previous ones are lost
         exec "${WORKDIR}/PHYEX/tools/testing.sh" $allargs
       fi
     fi
+    log 1 "Installing/updating PHYEX"
+    ${WORKDIR}/PHYEX/tools/INSTALL.sh --ALL
   fi
   . "${WORKDIR}/PHYEX/tools/env.sh"
 
