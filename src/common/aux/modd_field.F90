474de5b03cab098e71bd52c8fa74f09d54f52b09
Sébastien Riette <sebastien.riette@meteo.fr>
2024-03-27 17:51:06 +0100

    Suprpession of unused values in aux/modd_field.F90

--- src/common/aux/modd_field.F90
+++ src/common/aux/modd_field.F90
@@ -14,16 +14,8 @@ TYPE TFIELDMETADATA
   INTEGER            :: NGRID     = NGRIDUNKNOWN !Localization on the model grid
   INTEGER            :: NTYPE     = TYPEUNDEF !Datatype
   INTEGER            :: NDIMS     = 0  !Number of dimensions
-  INTEGER, DIMENSION(NMNHMAXDIMS) :: NDIMLIST = NMNHDIM_UNKNOWN ! List of dimensions of the data field
   !
-  INTEGER            :: NFILLVALUE =  -2147483647            !Fill value for integer fields
-  REAL               :: XFILLVALUE =  9.9692099683868690e+36 !Fill value for real fields
-  INTEGER            :: NVALIDMIN  = -2147483646 !Minimum valid value for integer fields
-  INTEGER            :: NVALIDMAX  =  2147483647 !Maximum valid value for integer fields
-  REAL               :: XVALIDMIN  = -1.E36 !Minimum valid value for real fields
-  REAL               :: XVALIDMAX  =  1.E36 !Maximum valid value for real fields
   CHARACTER(LEN=2)   :: CDIR      = '' !Type of the data field (XX,XY,--...)
-  CHARACTER(LEN=4)   :: CLBTYPE   = 'NONE' !Type of the lateral boundary (LBX,LBY,LBXU,LBYV)
   LOGICAL            :: LTIMEDEP  = .FALSE. !Is the field time-dependent?
 END TYPE TFIELDMETADATA
 END MODULE MODD_FIELD
