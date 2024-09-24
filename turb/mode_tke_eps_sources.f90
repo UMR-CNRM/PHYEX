c5d08922c2436715a6a71158aaf169ab43359492
Quentin Rodier <quentin.rodier@meteo.fr>
2024-09-24 10:35:25 +0200

    Quentin 24/09/2024: add back ifdef around mnh_undef(LOOP) and mnh_undef(OPENACC) because the macro is handled by CPP and not pyft

--- turb/mode_tke_eps_sources.f90
+++ turb/mode_tke_eps_sources.f90
@@ -150,8 +150,10 @@ USE MODI_LES_MEAN_SUBGRID_PHY
 USE MODE_TRIDIAG_TKE,          ONLY: TRIDIAG_TKE
 !
 ! These macro are handled by pft_tool.py --craybyPassDOCONCURRENT applied on Cray Rules
+#ifdef MNH_COMPILER_CCE
 !$mnh_undef(LOOP)
 !$mnh_undef(OPENACC)
+#endif
 !
 IMPLICIT NONE
 !
--- turb/mode_turb_hor_dyn_corr.f90
+++ turb/mode_turb_hor_dyn_corr.f90
@@ -106,8 +106,10 @@ USE MODI_SECOND_MNH
 USE MODE_MPPDB
 !
 ! These macro are handled by pft_tool.py --craybyPassDOCONCURRENT applied on Cray Rules
+#ifdef MNH_COMPILER_CCE
 !$mnh_undef(LOOP)
 !$mnh_undef(OPENACC)
+#endif
 !
 IMPLICIT NONE
 !
