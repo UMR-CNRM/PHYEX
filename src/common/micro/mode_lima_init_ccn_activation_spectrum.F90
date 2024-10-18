0c854fe6fd8b6f97950ac646da7a45d7cbccab4b
Benoît Vié <benoit.vie@meteo.fr>
2024-10-18 09:17:03 +0200

    check real arguments names for DOCTOR norm

--- src/common/micro/mode_lima_init_ccn_activation_spectrum.F90
+++ src/common/micro/mode_lima_init_ccn_activation_spectrum.F90
@@ -7,7 +7,7 @@ MODULE MODE_LIMA_INIT_CCN_ACTIVATION_SPECTRUM
   IMPLICIT NONE
 CONTAINS
 !     #############################################################
-  SUBROUTINE LIMA_INIT_CCN_ACTIVATION_SPECTRUM (CTYPE_CCN,XD,XSIGMA,XLIMIT_FACTOR,XK,XMU,XBETA,XKAPPA)
+  SUBROUTINE LIMA_INIT_CCN_ACTIVATION_SPECTRUM (CTYPE_CCN,PD,PSIGMA,PLIMIT_FACTOR,PK,PMU,PBETA,PKAPPA)
 !     #############################################################
 
 !!
@@ -45,38 +45,37 @@ IMPLICIT NONE
 !*       0.1   Declarations of dummy arguments : 
 !
 CHARACTER(LEN=*), INTENT(IN)  :: CTYPE_CCN          ! Aerosol type
-REAL,             INTENT(IN)  :: XD             ! Aerosol PSD modal diameter          
-REAL,             INTENT(IN)  :: XSIGMA         ! Aerosol PSD width
-REAL,             INTENT(OUT) :: XLIMIT_FACTOR  ! C/Naer
-REAL,             INTENT(OUT) :: XK             ! k
-REAL,             INTENT(OUT) :: XMU            ! mu
-REAL,             INTENT(OUT) :: XBETA          ! beta
-REAL,             INTENT(OUT) :: XKAPPA         ! kappa
+REAL,             INTENT(IN)  :: PD             ! Aerosol PSD modal diameter          
+REAL,             INTENT(IN)  :: PSIGMA         ! Aerosol PSD width
+REAL,             INTENT(OUT) :: PLIMIT_FACTOR  ! C/Naer
+REAL,             INTENT(OUT) :: PK             ! k
+REAL,             INTENT(OUT) :: PMU            ! mu
+REAL,             INTENT(OUT) :: PBETA          ! beta
+REAL,             INTENT(OUT) :: PKAPPA         ! kappa
 !
 !*       0.2   Declarations of local variables :
 !
-INTEGER, PARAMETER            :: M = 1000        ! Number of points (S,Nccn) used to fit the spectra
-INTEGER, PARAMETER            :: N = 3          ! Number of parameters to adjust
-REAL, DIMENSION(N)            :: PARAMS         ! Parameters to adjust by the LM algorithm (k, mu, beta)
-REAL, DIMENSION(M)            :: FVEC           ! Array to store the distance between theoretical and fitted spectra
+INTEGER, PARAMETER            :: IM = 1000        ! Number of points (S,Nccn) used to fit the spectra
+INTEGER, PARAMETER            :: IN = 3          ! Number of parameters to adjust
+REAL, DIMENSION(N)            :: ZPARAMS         ! Parameters to adjust by the LM algorithm (k, mu, beta)
+REAL, DIMENSION(M)            :: ZFVEC           ! Array to store the distance between theoretical and fitted spectra
 INTEGER                       :: IFLAG          ! 
-INTEGER                       :: INFO           ! 
-REAL                          :: TOL = 1.E-16   ! Fit precision required
+INTEGER                       :: IINFO           ! 
+REAL                          :: ZTOL = 1.E-16   ! Fit precision required
 !
 INTEGER                       :: II, IJ         ! Loop indices
 !
-REAL                          :: XW             ! 
-REAL                          :: XDDRY = 0.1E-6 ! Dry diameter for which to compute Scrit
-REAL                          :: XSCRIT         ! Scrit for dry diameter XDDRY
-REAL                          :: XMIN  = 0.1E-6 ! minimum diameter for root search (m)
-REAL                          :: XMAX  = 10.E-6 ! maximum diameter for root search (m)
-REAL                          :: XPREC = 1.E-8  ! precision wanted for root (m)
+REAL                          :: ZW             ! 
+REAL                          :: ZDDRY = 0.1E-6 ! Dry diameter for which to compute Scrit
+REAL                          :: ZSCRIT         ! Scrit for dry diameter ZDDRY
+REAL                          :: ZMIN  = 0.1E-6 ! minimum diameter for root search (m)
+REAL                          :: ZMAX  = 10.E-6 ! maximum diameter for root search (m)
+REAL                          :: ZPREC = 1.E-8  ! precision wanted for root (m)
 !
-!REAL                          :: XKAPPA         ! kappa coefficient
-REAL, DIMENSION(M)            :: XS             ! saturation ratio (S=1.01 for a 1% supersaturation)
-REAL, DIMENSION(M)            :: XDCRIT         ! critical diameters (m) for the chosen S values
-REAL, DIMENSION(M)            :: XNCCN          ! fraction of the aerosols larger than XDCRIT (ie activable)
-REAL, DIMENSION(1)            :: XT             ! temperature
+REAL, DIMENSION(M)            :: ZS             ! saturation ratio (S=1.01 for a 1% supersaturation)
+REAL, DIMENSION(M)            :: ZDCRIT         ! critical diameters (m) for the chosen S values
+REAL, DIMENSION(M)            :: ZNCCN          ! fraction of the aerosols larger than ZDCRIT (ie activable)
+REAL, DIMENSION(1)            :: ZT             ! temperature
 !
 !
 !-------------------------------------------------------------------------------
@@ -88,63 +87,63 @@ REAL, DIMENSION(1)            :: XT             ! temperature
 !
 SELECT CASE (CTYPE_CCN)
 CASE('NH42SO4','C') ! Ammonium sulfate
-   XKAPPA = 0.61
+   PKAPPA = 0.61
 CASE('NH4NO3')      ! Ammonium nitrate
-   XKAPPA = 0.67
+   PKAPPA = 0.67
 CASE('NaCl','M')    ! Sea Salt
-   XKAPPA = 1.28
+   PKAPPA = 1.28
 CASE('H2SO4')       ! Sulfuric acid
-   XKAPPA = 0.90
+   PKAPPA = 0.90
 CASE('NaNO3')       ! Sodium nitrate
-   XKAPPA = 0.88
+   PKAPPA = 0.88
 CASE('NaHSO4')      ! Sodium bisulfate
-   XKAPPA = 0.91
+   PKAPPA = 0.91
 CASE('Na2SO4')      ! Sodium sulfate
-   XKAPPA = 0.80
+   PKAPPA = 0.80
 CASE('NH43HSO42')   ! Letovicite (rare ammonium sulfate mineral)
-   XKAPPA = 0.65
+   PKAPPA = 0.65
 CASE('SOA')         ! Secondary organic aerosol (alpha-pinene, beta-pinene)
-   XKAPPA = 0.1
+   PKAPPA = 0.1
 CASE DEFAULT
-   XKAPPA = 1.
+   PKAPPA = 1.
 END SELECT
 !
-!XT = (/ 270., 271., 272., 273., 274., 275., 276., 277., 278., 279., 280., 281., 282., 283., 284., 285., 286., 287., 288., 289. /)
-XT = (/ 280. /)
+!ZT = (/ 270., 271., 272., 273., 274., 275., 276., 277., 278., 279., 280., 281., 282., 283., 284., 285., 286., 287., 288., 289. /)
+ZT = (/ 280. /)
 
 !
 ! Initialize supersaturation values (in %)
 !
-DO II=1, SIZE(XS)
-   XS(II)=EXP( LOG(10.**(-3.)) + REAL(II) / REAL(SIZE(XS)) * (LOG(10.**2.)-LOG(10.**(-3.))) )
+DO II=1, SIZE(ZS)
+   ZS(II)=EXP( LOG(10.**(-3.)) + REAL(II) / REAL(SIZE(ZS)) * (LOG(10.**2.)-LOG(10.**(-3.))) )
 END DO
 
-DO IJ=1, SIZE(XT)
+DO IJ=1, SIZE(ZT)
 !
 !*       2.     Compute Nccn(s) for several supersaturation values
 !	        --------------------------------------------------
 !
 ! Get the value of Scrit at Ddry=0.1 micron
 !
-   XDDRY  = XD
-   XMIN   = XD
-   XMAX   = XD*10.
-   XPREC  = XD/100.
-   XW     = 4 * 0.072 * XMV / XAVOGADRO / XBOLTZ / XT(IJ) / XRHOLW
-   XSCRIT = ZRIDDR(XMIN,XMAX,XPREC,XDDRY,XKAPPA,XT(IJ))                             ! wet diameter at Scrit
-   XSCRIT = (XSCRIT**3-XDDRY**3) * EXP(XW/XSCRIT) / (XSCRIT**3-(1-XKAPPA)*XDDRY**3) ! Saturation ratio at Scrit
-   XSCRIT = (XSCRIT - 1.) * 100.                                                    ! Scrit (in %)
+   ZDDRY  = PD
+   ZMIN   = PD
+   ZMAX   = PD*10.
+   ZPREC  = PD/100.
+   ZW     = 4 * 0.072 * XMV / XAVOGADRO / XBOLTZ / ZT(IJ) / XRHOLW
+   ZSCRIT = ZRIDDR(ZMIN,ZMAX,ZPREC,ZDDRY,PKAPPA,ZT(IJ))                             ! wet diameter at Scrit
+   ZSCRIT = (ZSCRIT**3-ZDDRY**3) * EXP(ZW/ZSCRIT) / (ZSCRIT**3-(1-PKAPPA)*ZDDRY**3) ! Saturation ratio at Scrit
+   ZSCRIT = (ZSCRIT - 1.) * 100.                                                    ! Scrit (in %)
 !
-! Get the XDCRIT values for XS using the approx.
+! Get the ZDCRIT values for ZS using the approx.
 ! ln(100*(Sw))~Dcrit^(-3/2) where Sw is in % (Sw=1 for a 1% supersaturation)
 !
-   XW = XDDRY * XSCRIT**0.66             ! "a" factor in Ddry_crit = a*S**-0.66
-   XDCRIT(:) = XW * XS(:)**(-0.66)       ! Ddry_crit for each value of S
+   ZW = ZDDRY * ZSCRIT**0.66             ! "a" factor in Ddry_crit = a*S**-0.66
+   ZDCRIT(:) = ZW * ZS(:)**(-0.66)       ! Ddry_crit for each value of S
 !
 ! Compute Nccn(S) as the incomplete integral of n(D) from 0 to Ddry_crit(S)
 !
-   DO II=1, SIZE(XS)
-      XNCCN(II) = 1- ( 0.5 + SIGN(0.5,XDCRIT(II)-XD) * GAMMA_INC(0.5,(LOG(XDCRIT(II)/XD)/SQRT(2.)/LOG(XSIGMA))**2) )
+   DO II=1, SIZE(ZS)
+      ZNCCN(II) = 1- ( 0.5 + SIGN(0.5,ZDCRIT(II)-PD) * GAMMA_INC(0.5,(LOG(ZDCRIT(II)/PD)/SQRT(2.)/LOG(PSIGMA))**2) )
    END DO
 !
 !-------------------------------------------------------------------------------
@@ -152,14 +151,14 @@ DO IJ=1, SIZE(XT)
 !*       3.     Compute C, k, mu, beta, using the Levenberg-Marquardt algorithm
 !	        ---------------------------------------------------------------
 !
-   PARAMS(1:3) = (/ 1., 1., 1000. /)
+   ZPARAMS(1:3) = (/ 1., 1., 1000. /)
    IFLAG = 1
-   call lmdif1 ( DISTANCE, M, N, PARAMS, FVEC, TOL, INFO )
+   call lmdif1 ( DISTANCE, IM, IN, ZPARAMS, ZFVEC, ZTOL, IINFO )
 !
-   XLIMIT_FACTOR = gamma(PARAMS(2))*PARAMS(3)**(PARAMS(1)/2)/gamma(1+PARAMS(1)/2)/gamma(PARAMS(2)-PARAMS(1)/2)
-   XK            = PARAMS(1)
-   XMU           = PARAMS(2)
-   XBETA         = PARAMS(3)
+   PLIMIT_FACTOR = gamma(ZPARAMS(2))*ZPARAMS(3)**(ZPARAMS(1)/2)/gamma(1+ZPARAMS(1)/2)/gamma(ZPARAMS(2)-ZPARAMS(1)/2)
+   PK            = ZPARAMS(1)
+   PMU           = ZPARAMS(2)
+   PBETA         = ZPARAMS(3)
 !
 END DO ! loop on temperatures
 !
@@ -172,7 +171,7 @@ CONTAINS
 !
 !------------------------------------------------------------------------------
 !
-  FUNCTION ZRIDDR(PX1,PX2,PXACC,XDDRY,XKAPPA,XT)  RESULT(PZRIDDR)
+  FUNCTION ZRIDDR(PX1,PX2,PXACC,PDDRY,PKAPPA,PT)  RESULT(PZRIDDR)
 !
 !
 !!****  *ZRIDDR* - iterative algorithm to find root of a function
@@ -223,71 +222,71 @@ IMPLICIT NONE
 !*       0.1 declarations of arguments and result
 !
 REAL,    INTENT(INOUT)  :: PX1, PX2, PXACC
-REAL,    INTENT(IN)     :: XDDRY, XKAPPA, XT
+REAL,    INTENT(IN)     :: PDDRY, PKAPPA, PT
 REAL                    :: PZRIDDR
 !
 !*       0.2 declarations of local variables
 !
 !
-INTEGER, PARAMETER      :: MAXIT=60
-REAL                    :: fh,fl, fm,fnew
-REAL                    :: s,xh,xl,xm,xnew
-INTEGER                 :: j
+INTEGER, PARAMETER      :: IMAXIT=60
+REAL                    :: zfh,zfl, zfm,zfnew
+REAL                    :: zs,zxh,zxl,zxm,zxnew
+INTEGER                 :: ij
 !
 PZRIDDR= 999999.
-fl     = DSDD(PX1,XDDRY,XKAPPA,XT)
-fh     = DSDD(PX2,XDDRY,XKAPPA,XT)
+zfl     = DSDD(PX1,PDDRY,PKAPPA,PT)
+zfh     = DSDD(PX2,PDDRY,PKAPPA,PT)
 !
-100 if ((fl > 0.0 .and. fh < 0.0) .or. (fl < 0.0 .and. fh > 0.0)) then
-      xl         = PX1
-      xh         = PX2
-      do j=1,MAXIT
-         xm     = 0.5*(xl+xh)
-         fm = DSDD(xm,XDDRY,XKAPPA,XT)
-         s      = sqrt(fm**2-fl*fh)
-         if (s == 0.0) then
+100 if ((zfl > 0.0 .and. zfh < 0.0) .or. (zfl < 0.0 .and. zfh > 0.0)) then
+      zxl         = PX1
+      zxh         = PX2
+      do ij=1,IMAXIT
+         zxm     = 0.5*(zxl+zxh)
+         zfm = DSDD(zxm,PDDRY,PKAPPA,PT)
+         zs      = sqrt(zfm**2-zfl*zfh)
+         if (zs == 0.0) then
             GO TO 101
          endif
-         xnew  = xm+(xm-xl)*(sign(1.0,fl-fh)*fm/s)
-         if (abs(xnew - PZRIDDR) <= PXACC) then
+         zxnew  = zxm+(zxm-zxl)*(sign(1.0,zfl-zfh)*zfm/zs)
+         if (abs(zxnew - PZRIDDR) <= PXACC) then
             GO TO 101 
          endif
-         PZRIDDR = xnew
-         fnew  = DSDD(PZRIDDR,XDDRY,XKAPPA,XT)
-         if (fnew == 0.0) then
+         PZRIDDR = zxnew
+         zfnew  = DSDD(PZRIDDR,PDDRY,PKAPPA,PT)
+         if (zfnew == 0.0) then
             GO TO 101
          endif
-         if (sign(fm,fnew) /= fm) then
-            xl    =xm
-            fl=fm
-            xh    =PZRIDDR
-            fh=fnew
-         else if (sign(fl,fnew) /= fl) then
-            xh    =PZRIDDR
-            fh=fnew
-         else if (sign(fh,fnew) /= fh) then
-            xl    =PZRIDDR
-            fl=fnew
+         if (sign(zfm,zfnew) /= zfm) then
+            zxl    =zxm
+            zfl=zfm
+            zxh    =PZRIDDR
+            zfh=zfnew
+         else if (sign(zfl,zfnew) /= zfl) then
+            zxh    =PZRIDDR
+            zfh=zfnew
+         else if (sign(zfh,zfnew) /= zfh) then
+            zxl    =PZRIDDR
+            zfl=zfnew
          else if (PX2 .lt. 0.05) then
             PX2 = PX2 + 1.0E-2
 !            PRINT*, 'PX2 ALWAYS too small, we put a greater one : PX2 =',PX2
-            fh   = DSDD(PX2,XDDRY,XKAPPA,XT)
+            zfh   = DSDD(PX2,PDDRY,PKAPPA,PT)
             go to 100
             STOP
          end if
-         if (abs(xh-xl) <= PXACC) then
+         if (abs(zxh-zxl) <= PXACC) then
             GO TO 101 
          endif
       end do
       STOP
-   else if (fl == 0.0) then
+   else if (zfl == 0.0) then
       PZRIDDR=PX1
-   else if (fh == 0.0) then
+   else if (zfh == 0.0) then
       PZRIDDR=PX2
    else if (PX2 .lt. 0.05) then
       PX2 = PX2 + 1.0E-2
 !      PRINT*, 'PX2 too small, we put a greater one : PX2 =',PX2
-      fh   = DSDD(PX2,XDDRY,XKAPPA,XT)
+      zfh   = DSDD(PX2,PDDRY,PKAPPA,PT)
       go to 100
    else
       PZRIDDR=0.0
@@ -298,7 +297,7 @@ fh     = DSDD(PX2,XDDRY,XKAPPA,XT)
 !
 !------------------------------------------------------------------------------
 !
-  FUNCTION DSDD(XD,XDDRY,XKAPPA, XT)  RESULT(DS)
+  FUNCTION DSDD(PD,PDDRY,PKAPPA, PT)  RESULT(ZDS)
 !!
 !!    PURPOSE
 !!    -------
@@ -338,20 +337,20 @@ fh     = DSDD(PX2,XDDRY,XKAPPA,XT)
 !
 !*       0.1 declarations of arguments and result
 !
-    REAL, INTENT(IN)  :: XD     ! supersaturation is already in no units
-    REAL, INTENT(IN)  :: XDDRY  ! supersaturation is already in no units
-    REAL, INTENT(IN)  :: XKAPPA ! supersaturation is already in no units
-    REAL, INTENT(IN)  :: XT     ! supersaturation is already in no units
+    REAL, INTENT(IN)  :: PD     ! supersaturation is already in no units
+    REAL, INTENT(IN)  :: PDDRY  ! supersaturation is already in no units
+    REAL, INTENT(IN)  :: PKAPPA ! supersaturation is already in no units
+    REAL, INTENT(IN)  :: PT     ! supersaturation is already in no units
 !
-    REAL              :: DS     ! result
+    REAL              :: ZDS     ! result
 !
 !*       0.2 declarations of local variables
 !
-    REAL              :: XA     ! factor inside the exponential
+    REAL              :: ZA     ! factor inside the exponential
 !    
-    XA = 4 * 0.072 * XMV / XAVOGADRO / XBOLTZ / XT / XRHOLW
-    DS = (XD**3-XDDRY**3) * (XD**3-(1-XKAPPA)*XDDRY**3) * XA - 3. * XKAPPA * XD**4 * XDDRY**3
-    DS = DS * EXP(XA/XD) / (XD**3-(1-XKAPPA)*XDDRY**3)**2
+    ZA = 4 * 0.072 * XMV / XAVOGADRO / XBOLTZ / PT / XRHOLW
+    ZDS = (PD**3-PDDRY**3) * (PD**3-(1-PKAPPA)*PDDRY**3) * ZA - 3. * PKAPPA * PD**4 * PDDRY**3
+    ZDS = ZDS * EXP(ZA/PD) / (PD**3-(1-PKAPPA)*PDDRY**3)**2
 !
 END FUNCTION DSDD
 !
@@ -360,7 +359,7 @@ END FUNCTION DSDD
 !*       7.     Functions used to fit the CCN activation spectra with C s**k F()
 !   	        ----------------------------------------------------------------
 !
-  SUBROUTINE DISTANCE(M,N,X,FVEC,IFLAG)
+  SUBROUTINE DISTANCE(KM,KN,PX,PFVEC,IFLAG)
 !!
 !!    PURPOSE
 !!    -------
@@ -396,41 +395,41 @@ END FUNCTION DSDD
 !
 !*       0.1 declarations of arguments and result
 !
-    integer, intent(in) :: M
-    integer, intent(in) :: N
-    real, intent(in) ::    X(N)
-    real, intent(out) ::    FVEC(M)
+    integer, intent(in) :: KM
+    integer, intent(in) :: KN
+    real, intent(in) ::    PX(KN)
+    real, intent(out) ::    PFVEC(KM)
     integer, intent(inout) :: IFLAG
 !
 !*       0.2 declarations of local variables
 !
-    integer I
-    real    C
+    integer II
+    real    ZC
     real    ZW
 !    
     ! print *, "X = ", X
-    IF ( ANY(X .LT.0.) .OR. X(1).gt.2*X(2)) THEN
-       FVEC(:) = 999999.
+    IF ( ANY(PX .LT.0.) .OR. PX(1).gt.2*PX(2)) THEN
+       PFVEC(:) = 999999.
     ELSE
-       C=gamma(X(2))*X(3)**(X(1)/2)/gamma(1+X(1)/2)/gamma(X(2)-X(1)/2)
-       DO I=1, M
-          ! XS in "no units", ie XS=0.01 for a 1% suersaturation
-          !       ZW= C * (XS(I)/100)**X(1) * HYPGEO(X(2),X(1)/2,X(1)/2+1,X(3),XS(I)/100)
-          ZW= C * (XS(I))**X(1) * HYPGEO(X(2),X(1)/2,X(1)/2+1,X(3),XS(I))
-!!$       IF (X(3)*(XS(I)/100)**2 .LT. 0.98) THEN
-!!$          CALL HYPSER(X(2),X(1)/2,X(1)/2+1,-X(3)*(XS(I)/100)**2,ZW2)
-!!$          print *, "args= ", X(2), X(1)/2, X(1)/2+1, -X(3)*(XS(I)/100)**2, " hypser = ", ZW2
+       ZC=gamma(PX(2))*PX(3)**(PX(1)/2)/gamma(1+PX(1)/2)/gamma(PX(2)-PX(1)/2)
+       DO II=1, KM
+          ! ZS in "no units", ie ZS=0.01 for a 1% suersaturation
+          !       ZW= C * (ZS(I)/100)**X(1) * HYPGEO(X(2),X(1)/2,X(1)/2+1,X(3),ZS(I)/100)
+          ZW= ZC * (ZS(II))**X(1) * HYPGEO(PX(2),PX(1)/2,PX(1)/2+1,PX(3),ZS(II))
+!!$       IF (X(3)*(ZS(I)/100)**2 .LT. 0.98) THEN
+!!$          CALL HYPSER(X(2),X(1)/2,X(1)/2+1,-X(3)*(ZS(I)/100)**2,ZW2)
+!!$          print *, "args= ", X(2), X(1)/2, X(1)/2+1, -X(3)*(ZS(I)/100)**2, " hypser = ", ZW2
 !!$          CALL HYPSER(27.288,0.82/2,0.82/2+1,-38726*(0.5/100)**2,ZW2)
 !!$          print *, "args= ", 27.288, 0.82/2, 0.82/2+1, -38726*(0.5/100)**2, " hypser = ", ZW2
 !!$       END IF
-          !       print *, I, XS(I), C, ZW, XNCCN(I)
-          IF ( ZW.GT.0. .AND. XNCCN(I).GT.0.) THEN
-             FVEC(I) = LOG(ZW) - LOG(XNCCN(I))
+          !       print *, I, ZS(I), C, ZW, ZNCCN(I)
+          IF ( ZW.GT.0. .AND. ZNCCN(II).GT.0.) THEN
+             PFVEC(II) = LOG(ZW) - LOG(ZNCCN(II))
           ELSE
-             FVEC(I) = 0.
+             PFVEC(II) = 0.
           END IF
-          !FVEC(I) = LOG(MAX(ZW,1.E-24)) - LOG(MAX(XNCCN(I),1.E-24))
-          !FVEC(I) = ZW - XNCCN(I)
+          !FVEC(I) = LOG(MAX(ZW,1.E-24)) - LOG(MAX(ZNCCN(I),1.E-24))
+          !FVEC(I) = ZW - ZNCCN(I)
        END DO
     END IF
 !    print *, "distance : ", SUM(FVEC*FVEC)
