455d64341c3af86952f2c125ed5a9c5018229977
Philippe WAUTELET <philippe.wautelet@cnrs.fr>
2024-11-19 14:52:41 +0100

    Philippe 19/11/2024: fix for mode_bl89.f90: remove nonexistent ZLM variable in present directive

--- turb/mode_bl89.f90
+++ turb/mode_bl89.f90
@@ -1,6 +1,6 @@
-!MNH_LIC Copyright 1994-2022 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 1994-2024 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
-!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
+!MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt
 !MNH_LIC for details. version 1.
 MODULE MODE_BL89
 IMPLICIT NONE
@@ -199,7 +199,7 @@ END IF
 !but algorithm must remain the same.
 !!!!!!!!!!!!
 !
-!$acc kernels present_cr(ZLWORK,ZINTE,ZLM)
+!$acc kernels present_cr(ZLWORK,ZINTE)
 !$acc loop independent collapse(2)
 DO JK=IKTB,IKTE
   DO JIJ=IIJB,IIJE
