b7426fee04b305f2df28d6a4593681cbb6c4e80c
Philippe WAUTELET <philippe.wautelet@cnrs.fr>
2024-12-20 08:30:11 +0100

    Philippe 20/12/2024: minor modifications

--- src/common/micro/momg.F90
+++ src/common/micro/momg.F90
@@ -1,4 +1,4 @@
-!MNH_LIC Copyright 1994-2014 CNRS, Meteo-France and Universite Paul Sabatier
+!MNH_LIC Copyright 1994-2024 CNRS, Meteo-France and Universite Paul Sabatier
 !MNH_LIC This is part of the Meso-NH software governed by the CeCILL-C licence
 !MNH_LIC version 1. See LICENSE, CeCILL-C_V1-en.txt and CeCILL-C_V1-fr.txt  
 !MNH_LIC for details. version 1.
@@ -57,6 +57,8 @@ END MODULE MODI_MOMG
 !     ##############################################
 !
 USE MODI_GAMMA
+!
+IMPLICIT NONE
 !
 IMPLICIT NONE
 !
