9c00416b38c5eeb0a3ffd6ae0ad1e518af135537
Sébastien Riette <sebastien.riette@meteo.fr>
2025-07-10 10:30:14 +0200

    Add a check on coding conventions

---
+++ tools/check_coding_conventions.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#set -x
+set -e
+set -o pipefail #abort if left command on a pipe fails
+
+#This script checks:
+# - the presence of IMPLICIT NONE
+# - that all dummy arguments are declared using the INTENT attribute
+
+PHYEXTOOLSDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+SOURCEDIR=$PHYEXTOOLSDIR/../src/common
+
+################################
+#### COMMAND LINE ARGUMENTS ####
+################################
+
+verbose=0
+function usage {
+  echo "Usage: $0 [-h] [-v]"
+  echo "-v  print non conforming messages"
+}
+while [ -n "$1" ]; do
+  case "$1" in
+    '-h') usage; exit;;
+    '-v') verbose=1;;
+    *) echo "Too many arguments"; exit 3;;
+  esac
+  shift
+done
+
+################
+#### CHECKS ####
+################
+
+JSONDIR=$(mktemp -d)
+trap "\rm -rf $JSONDIR" EXIT
+result=$(pyfortool_parallel --tree $SOURCEDIR --descTree $JSONDIR/tree.json \
+                            --wrapH \
+                            --checkIMPLICIT Warn --checkINTENT Warn 2>&1)
+if [ "$result" == "" ]; then
+  exit 0
+else
+  if [ $verbose -eq 1 ]; then
+    echo "$result"
+  fi
+  exit 2
+fi
