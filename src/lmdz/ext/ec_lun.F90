0adc186cce7a6d5304d287cfbaba140c2cc66fba
Sébastien Riette <sebastien.riette@meteo.fr>
2026-05-27 08:37:05 +0200

    Remove LMDZ version of mode_msg

---
+++ src/lmdz/ext/ec_lun.F90
@@ -0,0 +1,7 @@
+! PHYEX needs this module to compile
+! This module is normally part of the fiat package
+! Here is a simplified version
+MODULE EC_LUN
+USE PRINT_CONTROL_MOD, ONLY : NULOUT => LUNOUT
+IMPLICIT NONE
+END MODULE EC_LUN
