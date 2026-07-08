8f10934d9b239d88f220a30ab998afd9f0f72038
Philippe Marguinaud <philippe.marguinaud@meteo.fr>
2026-07-08 10:20:10 +0000

    Compile acvppkf_parallel.F90 & aro_rain_ice_parallel.F90

--- src/common/conv/shallow_convection.F90
+++ src/common/conv/shallow_convection.F90
@@ -207,14 +207,15 @@ ICONV = COUNT(GTRIG1(D%NIJB:D%NIJE))
 IF(ICONV==0)THEN
   ! Do nothing if there are no selected columns
 ELSE IF (ICONV < D%NIJT*9/10) THEN
-  CALL SHALLOW_CONVECTION_PART2_SELECT(CVP_SHAL, CVPEXT, CST, D, NSV, CONVPAR, KICE, &
-                                       OSETTADJ, PTADJS, PPABST, PZZ, PTT,  &
-                                       PRVT, PRCT, PRIT, OCH1CONV, KCH1,    &
-                                       PCH1, ZRDOCP, ZTHT, ZSTHV, ZSTHES,   &
-                                       ISDPL, ISPBL, ISLCL, ZSTHLCL, ZSTLCL,&
-                                       ZSRVLCL, ZSWLCL, ZSZLCL, ZSTHVELCL,  &
-                                       GTRIG1, PUMF, PTTEN, PRVTEN, PRCTEN, &
-                                       PRITEN, KCLTOP, KCLBAS, PCH1TEN, ICONV)
+  CALL SHALLOW_CONVECTION_PART2_SELECT &
+                             & (CVP_SHAL, CVPEXT, CST, D, NSV, CONVPAR, KICE, &
+                                OSETTADJ, PTADJS, PPABST, PZZ, PTT,  &
+                                PRVT, PRCT, PRIT, OCH1CONV, KCH1,    &
+                                PCH1, ZRDOCP, ZTHT, ZSTHV, ZSTHES,   &
+                                ISDPL, ISPBL, ISLCL, ZSTHLCL, ZSTLCL,&
+                                ZSRVLCL, ZSWLCL, ZSZLCL, ZSTHVELCL,  &
+                                GTRIG1, PUMF, PTTEN, PRVTEN, PRCTEN, &
+                                PRITEN, KCLTOP, KCLBAS, PCH1TEN)
 ELSE
   CALL SHALLOW_CONVECTION_PART2 &
                              & (CVP_SHAL, CVPEXT, CST, D, NSV, CONVPAR, KICE, &
