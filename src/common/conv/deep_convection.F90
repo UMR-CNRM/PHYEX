8f31889713d8cf87bacdc9206ee52563466d0319
Sébastien Riette <sebastien.riette@meteo.fr>
2026-09-03 16:48:46 +0200

    BF for dimensionning in chem_transport when called from deep comvection

--- src/common/conv/deep_convection.F90
+++ src/common/conv/deep_convection.F90
@@ -340,6 +340,7 @@ REAL, DIMENSION(:,:),ALLOCATABLE   :: ZWORK4, ZWORK4C
 REAL, DIMENSION(:,:),ALLOCATABLE   :: ZZZ, ZRHODREF
 REAL, DIMENSION(:),ALLOCATABLE     :: ZIC_RATE,ZCG_RATE
 !
+TYPE(DIMPHYEX_T) :: D_
 REAL(KIND=JPHOOK) :: ZHOOK_HANDLE
 !
 !-------------------------------------------------------------------------------
@@ -1124,7 +1125,11 @@ IF ( ICONV1 > 0 )  THEN
 !
     ELSE
 !
-      CALL CONVECT_CHEM_TRANSPORT( CVPEXT, D, TNSV, CST, KCH1, ZCH1, ZCH1C,      &
+      D_=D
+      D_%NIJT=ICONV
+      D_%NIJB=1
+      D_%NIJE=ICONV
+      CALL CONVECT_CHEM_TRANSPORT( CVPEXT, D_, TNSV, CST, KCH1, ZCH1, ZCH1C,      &
                                    IDPL, IPBL, ILCL, ICTL, ILFS, IDBL,  &
                                    ZUMF, ZUER, ZUDR, ZDMF, ZDER, ZDDR,  &
                                    ZTIMEC, ZDXDY, ZMIXF, ZLMASS, ZWSUB, &
